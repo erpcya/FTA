0c60e90c0c04dc2c480894cd37f8925125900e49
Yamel Senih <yamelsenih@gmail.com>
2013-07-17 10:29:54 -0530

    Change Model and Columns

--- migration/oracle/029Change_Table_And_Windows_oracle.sql
+++ migration/oracle/029Change_Table_And_Windows_oracle.sql
@@ -68,11 +68,6 @@ UPDATE AD_Column SET IsIdentifier='N',Updated=TO_DATE('2013-07-15 21:38:08','YYY
 UPDATE AD_Column SET IsIdentifier='N',Updated=TO_DATE('2013-07-15 21:38:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=65916
 ;
 
--- Jul 15, 2013 9:39:50 PM VET
--- SFAndroid Contribution
-INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,66350,469,0,10,53512,'Name',TO_DATE('2013-07-15 21:39:49','YYYY-MM-DD HH24:MI:SS'),100,'Alphanumeric identifier of the entity','U',60,'The name of an entity (record) is used as an default search option in addition to the search key. The name is up to 60 characters in length.','Y','Y','N','N','N','N','N','N','N','N','N','N','Y','Name',0,TO_DATE('2013-07-15 21:39:49','YYYY-MM-DD HH24:MI:SS'),100,0)
-;
-
 -- Jul 15, 2013 9:41:18 PM VET
 -- SFAndroid Contribution
 INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,66351,469,0,10,53513,'Name',TO_DATE('2013-07-15 21:41:17','YYYY-MM-DD HH24:MI:SS'),100,'Alphanumeric identifier of the entity','ECA01',60,'The name of an entity (record) is used as an default search option in addition to the search key. The name is up to 60 characters in length.','Y','Y','N','N','N','Y','N','Y','N','N','N','N','Y','Name',0,TO_DATE('2013-07-15 21:41:17','YYYY-MM-DD HH24:MI:SS'),100,0)
