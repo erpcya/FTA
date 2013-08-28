3dc3221b8a5db6e7c622d6153c424b3f047c5f9b
Yamel Senih <Yamel Senih@localhost>
2013-08-28 16:50:59 -0530

    Add Model Class for DocAction Credit Definition

--- base/org/spin/model/I_FTA_CreditDefinitionLine.java
+++ base/org/spin/model/I_FTA_CreditDefinitionLine.java
@@ -122,6 +122,19 @@ public interface I_FTA_CreditDefinitionLine
 
 	public I_C_UOM getC_UOM() throws RuntimeException;
 
+    /** Column name Description */
+    public static final String COLUMNNAME_Description = "Description";
+
+	/** Set Description.
+	  * Optional short description of the record
+	  */
+	public void setDescription (String Description);
+
+	/** Get Description.
+	  * Optional short description of the record
+	  */
+	public String getDescription();
+
     /** Column name FTA_CreditDefinition_ID */
     public static final String COLUMNNAME_FTA_CreditDefinition_ID = "FTA_CreditDefinition_ID";
 
@@ -154,6 +167,19 @@ public interface I_FTA_CreditDefinitionLine
 	  * The record is active in the system
 	  */
 	public boolean isActive();
+
+    /** Column name Line */
+    public static final String COLUMNNAME_Line = "Line";
+
+	/** Set Line No.
+	  * Unique line for this document
+	  */
+	public void setLine (int Line);
+
+	/** Get Line No.
+	  * Unique line for this document
+	  */
+	public int getLine();
 
     /** Column name M_Product_Category_ID */
     public static final String COLUMNNAME_M_Product_Category_ID = "M_Product_Category_ID";
