3a2374e8cc61d875c29235ef8059fa9f2c2a8112
Carlos Parada <carlosaparadam@gmail.com>
2013-09-10 18:15:02 -0530

    Correct Process Farming Allocation

---
+++ base/org/spin/model/MFTARecordWeight.java
@@ -0,0 +1,557 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * Copyright (C) 2003-2013 E.R.P. Consultores y Asociados, C.A.               *
+ * All Rights Reserved.                                                       *
+ * Contributor(s): Yamel Senih www.erpconsultoresyasociados.com               *
+ *****************************************************************************/
+package org.spin.model;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.Properties;
+
+import org.compiere.model.MDocType;
+import org.compiere.model.MInOut;
+import org.compiere.model.MInOutLine;
+import org.compiere.model.MOrder;
+import org.compiere.model.MOrderLine;
+import org.compiere.model.MPeriod;
+import org.compiere.model.MStorage;
+import org.compiere.model.MWarehouse;
+import org.compiere.model.ModelValidationEngine;
+import org.compiere.model.ModelValidator;
+import org.compiere.process.DocAction;
+import org.compiere.process.DocOptions;
+import org.compiere.process.DocumentEngine;
+import org.compiere.util.DB;
+import org.compiere.util.Env;
+import org.compiere.util.Msg;
+
+/**
+ * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a>
+ *
+ */
+public class MFTARecordWeight extends X_FTA_RecordWeight implements DocAction, DocOptions {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5574275427824428940L;
+
+	/**
+	 * *** Constructor ***
+	 * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a> 27/08/2013, 14:47:32
+	 * @param ctx
+	 * @param FTA_RecordWeight_ID
+	 * @param trxName
+	 */
+	public MFTARecordWeight(Properties ctx, int FTA_RecordWeight_ID,
+			String trxName) {
+		super(ctx, FTA_RecordWeight_ID, trxName);
+	}
+
+	/**
+	 * *** Constructor ***
+	 * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a> 27/08/2013, 14:47:32
+	 * @param ctx
+	 * @param rs
+	 * @param trxName
+	 */
+	public MFTARecordWeight(Properties ctx, ResultSet rs, String trxName) {
+		super(ctx, rs, trxName);
+	}
+
+	/**
+	 * 	Get Document Info
+	 *	@return document info (untranslated)
+	 */
+	public String getDocumentInfo()
+	{
+		MDocType dt = MDocType.get(getCtx(), 0);
+		return dt.getName() + " " + getDocumentNo();
+	}	//	getDocumentInfo
+
+	/**
+	 * 	Create PDF
+	 *	@return File or null
+	 */
+	public File createPDF ()
+	{
+		try
+		{
+			File temp = File.createTempFile(get_TableName()+get_ID()+"_", ".pdf");
+			return createPDF (temp);
+		}
+		catch (Exception e)
+		{
+			log.severe("Could not create PDF - " + e.getMessage());
+		}
+		return null;
+	}	//	getPDF
+
+	/**
+	 * 	Create PDF file
+	 *	@param file output file
+	 *	@return file if success
+	 */
+	public File createPDF (File file)
+	{
+	//	ReportEngine re = ReportEngine.get (getCtx(), ReportEngine.INVOICE, getC_Invoice_ID());
+	//	if (re == null)
+			return null;
+	//	return re.getPDF(file);
+	}	//	createPDF
+
+	
+	/**************************************************************************
+	 * 	Process document
+	 *	@param processAction document action
+	 *	@return true if performed
+	 */
+	public boolean processIt (String processAction)
+	{
+		m_processMsg = null;
+		DocumentEngine engine = new DocumentEngine (this, getDocStatus());
+		return engine.processIt (processAction, getDocAction());
+	}	//	processIt
+	
+	/**	Process Message 			*/
+	private String		m_processMsg = null;
+	/**	Just Prepared Flag			*/
+	private boolean		m_justPrepared = false;
+
+	/**
+	 * 	Unlock Document.
+	 * 	@return true if success 
+	 */
+	public boolean unlockIt()
+	{
+		log.info("unlockIt - " + toString());
+	//	setProcessing(false);
+		return true;
+	}	//	unlockIt
+	
+	/**
+	 * 	Invalidate Document
+	 * 	@return true if success 
+	 */
+	public boolean invalidateIt()
+	{
+		log.info("invalidateIt - " + toString());
+	//	setDocAction(DOCACTION_Prepare);
+		return true;
+	}	//	invalidateIt
+	
+	/**
+	 *	Prepare Document
+	 * 	@return new status (In Progress or Invalid) 
+	 */
+	public String prepareIt()
+	{
+		log.info(toString());
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this, ModelValidator.TIMING_BEFORE_PREPARE);
+		if (m_processMsg != null)
+			return DocAction.STATUS_Invalid;
+		
+		MDocType dt = MDocType.get(getCtx(), getC_DocType_ID());
+
+		//	Std Period open?
+		if (!MPeriod.isOpen(getCtx(), getDateDoc(), dt.getDocBaseType(), getAD_Org_ID()))
+		{
+			m_processMsg = "@PeriodClosed@";
+			return DocAction.STATUS_Invalid;
+		}
+		
+		//	Add up Amounts
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this, ModelValidator.TIMING_AFTER_PREPARE);
+		if (m_processMsg != null)
+			return DocAction.STATUS_Invalid;
+		m_justPrepared = true;
+		if (!DOCACTION_Complete.equals(getDocAction()))
+			setDocAction(DOCACTION_Complete);
+		return DocAction.STATUS_InProgress;
+	}	//	prepareIt
+	
+	/**
+	 * 	Approve Document
+	 * 	@return true if success 
+	 */
+	public boolean  approveIt()
+	{
+		log.info("approveIt - " + toString());
+		setIsApproved(true);
+		return true;
+	}	//	approveIt
+	
+	/**
+	 * 	Reject Approval
+	 * 	@return true if success 
+	 */
+	public boolean rejectIt()
+	{
+		log.info("rejectIt - " + toString());
+		setIsApproved(false);
+		return true;
+	}	//	rejectIt
+	
+	/**
+	 * 	Complete Document
+	 * 	@return new status (Complete, In Progress, Invalid, Waiting ..)
+	 */
+	public String completeIt()
+	{
+		//	Re-Check
+		if (!m_justPrepared)
+		{
+			String status = prepareIt();
+			if (!DocAction.STATUS_InProgress.equals(status))
+				return status;
+		}
+
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this, ModelValidator.TIMING_BEFORE_COMPLETE);
+		if (m_processMsg != null)
+			return DocAction.STATUS_Invalid;
+		
+		//	Valid Weight
+		String status = validWeight();
+		if(m_processMsg != null)
+			return status;
+		
+		//	Implicit Approval
+		if (!isApproved())
+			approveIt();
+		log.info(toString());		
+		
+		//	User Validation
+		String valid = ModelValidationEngine.get().fireDocValidate(this, ModelValidator.TIMING_AFTER_COMPLETE);
+		if (valid != null)
+		{
+			m_processMsg = valid;
+			return DocAction.STATUS_Invalid;
+		}
+		setDefiniteDocumentNo();
+
+		setProcessed(true);
+		setDocAction(DOCACTION_Close);
+		return DocAction.STATUS_Completed;
+	}	//	completeIt
+	
+	/**
+	 * Valid Weight
+	 * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a> 09/09/2013, 14:27:34
+	 * @return
+	 * @return String
+	 */
+	private String validWeight(){
+		//	Valid Weight
+		if (getGrossWeight() == null
+				|| getGrossWeight().equals(Env.ZERO)) {
+			m_processMsg = "@GrossWeight@ = @0@";
+			return (!isSOTrx()? DocAction.STATUS_Invalid: DocAction.STATUS_InProgress);
+		} else if(getTareWeight() == null
+				|| getTareWeight().equals(Env.ZERO)) {
+			m_processMsg = "@TareWeight@ = @0@";
+			return (!isSOTrx()? DocAction.STATUS_InProgress: DocAction.STATUS_Invalid);
+		} else if(getNetWeight() == null
+				|| getNetWeight().equals(Env.ZERO)) {
+			m_processMsg = "@NetWeight@ = @0@";
+			return DocAction.STATUS_Invalid;
+		} else if(getNetWeight().compareTo(Env.ZERO) < 0) {
+			m_processMsg = "@NetWeight@ < @0@";
+			return DocAction.STATUS_Invalid;
+		}
+		return null;
+	}
+	
+	
+	/**
+	 * 	Set the definite document number after completed
+	 */
+	private void setDefiniteDocumentNo() {
+		MDocType dt = MDocType.get(getCtx(), getC_DocType_ID());
+		if (dt.isOverwriteDateOnComplete()) {
+			setDateDoc(new Timestamp (System.currentTimeMillis()));
+		}
+		if (dt.isOverwriteSeqOnComplete()) {
+			String value = null;
+			int index = p_info.getColumnIndex("C_DocType_ID");
+			if (index == -1)
+				index = p_info.getColumnIndex("C_DocTypeTarget_ID");
+			if (index != -1)		//	get based on Doc Type (might return null)
+				value = DB.getDocumentNo(get_ValueAsInt(index), get_TrxName(), true);
+			if (value != null) {
+				setDocumentNo(value);
+			}
+		}
+	}
+
+	/**
+	 * 	Void Document.
+	 * 	Same as Close.
+	 * 	@return true if success 
+	 */
+	public boolean voidIt()
+	{
+		log.info("voidIt - " + toString());
+		// Before Void
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_BEFORE_VOID);
+		if (m_processMsg != null)
+			return false;
+		
+		addDescription(Msg.getMsg(getCtx(), "Voided"));
+
+		// After Void
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_AFTER_VOID);
+		if (m_processMsg != null)
+			return false;
+
+		setProcessed(true);
+        setDocAction(DOCACTION_None);
+		return true;
+	}	//	voidIt
+	
+	/**
+	 * 	Close Document.
+	 * 	Cancel not delivered Qunatities
+	 * 	@return true if success 
+	 */
+	public boolean closeIt()
+	{
+		log.info("closeIt - " + toString());
+		// Before Close
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_BEFORE_CLOSE);
+		if (m_processMsg != null)
+			return false;
+		// After Close
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_AFTER_CLOSE);
+		if (m_processMsg != null)
+			return false;
+
+		return true;
+	}	//	closeIt
+	
+	/**
+	 * 	Reverse Correction
+	 * 	@return true if success 
+	 */
+	public boolean reverseCorrectIt()
+	{
+		log.info("reverseCorrectIt - " + toString());
+		// Before reverseCorrect
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_BEFORE_REVERSECORRECT);
+		if (m_processMsg != null)
+			return false;
+
+		// After reverseCorrect
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_AFTER_REVERSECORRECT);
+		if (m_processMsg != null)
+			return false;
+
+		return false;
+	}	//	reverseCorrectionIt
+	
+	/**
+	 * 	Reverse Accrual - none
+	 * 	@return true if success 
+	 */
+	public boolean reverseAccrualIt()
+	{
+		log.info("reverseAccrualIt - " + toString());
+		// Before reverseAccrual
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_BEFORE_REVERSEACCRUAL);
+		if (m_processMsg != null)
+			return false;
+
+		// After reverseAccrual
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_AFTER_REVERSEACCRUAL);
+		if (m_processMsg != null)
+			return false;
+
+		return false;
+	}	//	reverseAccrualIt
+	
+	/** 
+	 * 	Re-activate
+	 * 	@return true if success 
+	 */
+	public boolean reActivateIt()
+	{
+		log.info("reActivateIt - " + toString());
+	//	setProcessed(false);
+		if (reverseCorrectIt())
+			return true;
+		return false;
+	}	//	reActivateIt
+	
+	
+	/*************************************************************************
+	 * 	Get Summary
+	 *	@return Summary of Document
+	 */
+	public String getSummary()
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append(getDocumentNo());
+		//	 - Description
+		if (getDescription() != null && getDescription().length() > 0)
+			sb.append(" - ").append(getDescription());
+		return sb.toString();
+	}	//	getSummary
+
+	/**
+	 * 	Get Document no
+	 *	@return Document No
+	 */
+	public String getDocumentNo()
+	{
+		return "-";
+	}	//	getDocumentNo
+
+	/**
+	 * 	Get Process Message
+	 *	@return clear text error message
+	 */
+	public String getProcessMsg()
+	{
+		return m_processMsg;
+	}	//	getProcessMsg
+	
+	/**
+	 * 	Get Document Owner (Responsible)
+	 *	@return AD_User_ID
+	 */
+	public int getDoc_User_ID()
+	{
+	//	return getSalesRep_ID();
+		return 0;
+	}	//	getDoc_User_ID
+
+	/**
+	 * 	Get Document Approval Amount
+	 *	@return amount
+	 */
+	public BigDecimal getApprovalAmt()
+	{
+		return null;	//getTotalLines();
+	}	//	getApprovalAmt
+	
+	/**
+	 * 	Get Document Currency
+	 *	@return C_Currency_ID
+	 */
+	public int getC_Currency_ID()
+	{
+	//	MPriceList pl = MPriceList.get(getCtx(), getM_PriceList_ID());
+	//	return pl.getC_Currency_ID();
+		return 0;
+	}	//	getC_Currency_ID
+	
+	/**
+     *  Add to Description
+     *  @param description text
+     */
+    public void addDescription (String description)
+    {
+        String desc = getDescription();
+        if (desc == null)
+            setDescription(description);
+        else
+            setDescription(desc + " | " + description);
+    }   //  addDescription
+    
+    /**
+	 * 	Document Status is Complete or Closed
+	 *	@return true if CO, CL or RE
+	 */
+	public boolean isComplete()
+	{
+		String ds = getDocStatus();
+		return DOCSTATUS_Completed.equals(ds)
+			|| DOCSTATUS_Closed.equals(ds)
+			|| DOCSTATUS_Reversed.equals(ds);
+	}	//	isComplete
+
+	@Override
+	public int customizeValidActions(String docStatus, Object processing,
+			String orderType, String isSOTrx, int AD_Table_ID,
+			String[] docAction, String[] options, int index) {
+		//	Valid Document Action
+		if (AD_Table_ID == Table_ID){
+			if (docStatus.equals(DocumentEngine.STATUS_Drafted)
+					|| docStatus.equals(DocumentEngine.STATUS_InProgress)
+					|| docStatus.equals(DocumentEngine.STATUS_Invalid))
+				{
+					options[index++] = DocumentEngine.ACTION_Prepare;
+				}
+				//	Complete                    ..  CO
+				else if (docStatus.equals(DocumentEngine.STATUS_Completed))
+				{
+					options[index++] = DocumentEngine.ACTION_Void;
+					options[index++] = DocumentEngine.ACTION_ReActivate;
+				}
+		}
+		
+		return index;
+	}
+	
+	private MInOut createShipment(MOrder order, MDocType dt, Timestamp movementDate)
+	{
+		log.info("For " + dt);
+		MInOut shipment = new MInOut (order, dt.getC_DocTypeShipment_ID(), movementDate);
+	//	shipment.setDateAcct(getDateAcct());
+		if (!shipment.save(get_TrxName()))
+		{
+			m_processMsg = "Could not create Shipment";
+			return null;
+		}
+		
+		MOrderLine[] oLines = order.getLines(true, null);
+		MOrderLine oLine = oLines[0];
+			//
+		MInOutLine ioLine = new MInOutLine(shipment);
+			//	Qty = Ordered - Delivered
+		BigDecimal MovementQty = oLine.getQtyOrdered().subtract(oLine.getQtyDelivered()); 
+			//	Location
+		int M_Locator_ID = MStorage.getM_Locator_ID (oLine.getM_Warehouse_ID(), 
+				oLine.getM_Product_ID(), oLine.getM_AttributeSetInstance_ID(), 
+				MovementQty, get_TrxName());
+		if (M_Locator_ID == 0)		//	Get default Location
+		{
+			MWarehouse wh = MWarehouse.get(getCtx(), oLine.getM_Warehouse_ID());
+			M_Locator_ID = wh.getDefaultLocator().getM_Locator_ID();
+		}
+		//
+		ioLine.setOrderLine(oLine, M_Locator_ID, MovementQty);
+		ioLine.setQty(MovementQty);
+		if (oLine.getQtyEntered().compareTo(oLine.getQtyOrdered()) != 0)
+			ioLine.setQtyEntered(MovementQty
+				.multiply(oLine.getQtyEntered())
+				.divide(oLine.getQtyOrdered(), 6, BigDecimal.ROUND_HALF_UP));
+		if (!ioLine.save(get_TrxName()))
+		{
+			m_processMsg = "Could not create Shipment Line";
+			return null;
+		}
+		//	Manually Process Shipment
+		shipment.processIt(DocAction.ACTION_Complete);
+		shipment.saveEx(get_TrxName());
+		if (!DOCSTATUS_Completed.equals(shipment.getDocStatus()))
+		{
+			m_processMsg = "@M_InOut_ID@: " + shipment.getProcessMsg();
+			return null;
+		}
+		return shipment;
+	}	//	createShipment
+}
---
+++ base/org/spin/process/FarmingAllocation.java
@@ -0,0 +1,141 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * Copyright (C) 2003-2013 E.R.P. Consultores y Asociados.                    *
+ * All Rights Reserved.                                                       *
+ * Contributor(s): Carlos Parada www.erpconsultoresyasociados.com             *
+ *****************************************************************************/
+
+package org.spin.process;
+
+import java.util.List;
+import java.util.Properties;
+
+import org.compiere.model.Query;
+import org.compiere.process.ProcessInfoParameter;
+import org.compiere.process.SvrProcess;
+import org.compiere.util.CLogger;
+import org.compiere.util.Env;
+import org.spin.model.MFTACreditDefinition;
+import org.spin.model.MFTACreditDefinitionLine;
+import org.spin.model.MFTAFarmerCredit;
+import org.spin.model.MFTAFarming;
+
+
+/**
+ * @author <a href="mailto:carlosaparadam@gmail.com">Carlos Parada</a>
+ *	<br> Allocate Farming to Credit
+ */
+public class FarmingAllocation extends SvrProcess {
+
+	@Override
+	protected void prepare() {
+		// TODO Auto-generated method stub
+		for (ProcessInfoParameter para:getParameter()){
+			String name = para.getParameterName();
+			if (para.getParameter() == null)
+				;
+			else if (name.equals("FTA_Farming_ID"))
+				m_FTA_Farming_ID = para.getParameterAsInt();
+		}
+		
+	}
+
+	@Override
+	protected String doIt() throws Exception {
+		// TODO Auto-generated method stub
+		
+		//Current Farmer Credit
+		MFTAFarmerCredit credit = new MFTAFarmerCredit(ctx, getRecord_ID(), get_TrxName());
+		
+		//Credit Definition
+		MFTACreditDefinition creditdef= new  MFTACreditDefinition(ctx, credit.getFTA_CreditDefinition_ID(), get_TrxName());
+		
+		
+		
+		filter.append((m_FTA_Farming_ID==0?
+				"/*Framing Category Equals Credit Definition Category */ " 
+				+"FTA_Farming.Category_ID In (Select CD.Category_ID From FTA_CreditDefinition CD Where FTA_CreditDefinition_ID="+credit.getFTA_CreditDefinition_ID()+") " 
+				+"/*Business Partner Credit Equals to Farm*/ "
+				+"And Exists(Select 1 From FTA_FarmDivision fd "
+				+"Inner Join FTA_Farm f On f.FTA_Farm_ID=fd.FTA_Farm_ID Where f.C_BPartner_ID="+credit.getC_BPartner_ID()+" And FTA_Farming.FTA_FarmDivision_ID=fd.FTA_FarmDivision_ID ) "
+				+"/*Not In Another Credit*/ "
+				+"And (FTA_Farming.FTA_FarmerCredit_ID Is Null Or FTA_Farming.FTA_FarmerCredit_ID="+credit.getFTA_FarmerCredit_ID()+")":
+				"FTA_Farming_ID="+m_FTA_Farming_ID));
+		
+		log.fine(filter.toString());
+
+		//Get list of farming from farmer
+		List<MFTAFarming> farmings = new Query(ctx,MFTAFarming.Table_Name,filter.toString(),get_TrxName())
+										.setOnlyActiveRecords(true)
+										.list();
+		
+		for (MFTAFarming farming:farmings)
+		{
+			//Set Farmer Credit
+			if(farming.getFTA_FarmerCredit_ID()==0)
+			{
+				farming.setFTA_FarmerCredit_ID(credit.getFTA_FarmerCredit_ID());
+				credit.setQty(credit.getQty().add(farming.getArea()));
+				
+				//Set Amt from Credit Definition
+				for (MFTACreditDefinitionLine creditdefline:creditdef.getLines(false) )
+				{
+					credit.setAmt(
+							credit.getAmt().add(
+									farming.getArea().multiply(creditdefline.getPrice())
+													 .divide(creditdefline.getQty())
+											    )
+								 );
+				}
+			}
+			else
+			{
+				farming.setFTA_FarmerCredit_ID(0);
+				credit.setQty(credit.getQty().subtract(farming.getArea()));
+				
+				//Set Amt from Credit Definition
+				for (MFTACreditDefinitionLine creditdefline:creditdef.getLines(false) )
+				{
+					credit.setAmt(
+							credit.getAmt().subtract(
+									farming.getArea().multiply(creditdefline.getPrice())
+													 .divide(creditdefline.getQty())
+											    )
+								 );
+				}
+			}
+			
+			farming.save(get_TrxName());
+			credit.save(get_TrxName());
+			
+			m_Updted++;
+		}
+		
+		return "@Upated@="+m_Updted;
+	}
+
+	/** Farming	 */
+	private int m_FTA_Farming_ID =0;
+	
+	/** Logger	 */
+	private CLogger log = CLogger.getCLogger(FarmingAllocation.class);
+	
+	/** Sql Filter	 */
+	private StringBuffer filter = new StringBuffer();
+	
+	/** Context	 */
+	private Properties ctx = Env.getCtx();
+	
+	/** Records Updated	 */
+	private int m_Updted=0;
+}//FarmingAllocation
