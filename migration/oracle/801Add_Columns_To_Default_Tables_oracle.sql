7d6b182dd76aabcc8e8765043352e7a02f886d6d
Dixon Martinez <Dixon Martinez@localhost>
2013-09-11 08:01:08 -0530

    Add columns to default tables and upgrade FTA_FarmerLiquidation.

---
+++ migration/oracle/801Add_Columns_To_Default_Tables_oracle.sql
@@ -0,0 +1,10 @@
+-- Sep 11, 2013 8:57:26 AM VET
+-- SFAndroid Contribution
+ALTER TABLE C_DocType ADD C_DocTypeOrder_ID NUMBER(10) DEFAULT NULL 
+;
+
+-- Sep 11, 2013 8:57:59 AM VET
+-- SFAndroid Contribution
+ALTER TABLE C_Order ADD FTA_FarmerCredit_ID NUMBER(10) DEFAULT NULL 
+;
+
