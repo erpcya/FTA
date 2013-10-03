433704c257ab8bd7822f0d8264e610c06c5861db
Yamel Senih <Yamel Senih@localhost>
2013-10-03 15:57:54 -0530

    Change SQL

--- migration/oracle/039Add_Change_Column_oracle.sql
+++ migration/oracle/039Add_Change_Column_oracle.sql
@@ -113,29 +113,9 @@ DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=66532
 DELETE FROM AD_Column WHERE AD_Column_ID=66532
 ;
 
--- Aug 12, 2013 11:14:02 AM VET
--- SFAndroid Contribution
-INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,Description,EntityType,Help,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,56105,0,'Beneficiary_ID',TO_DATE('2013-08-12 11:13:59','YYYY-MM-DD HH24:MI:SS'),100,'Business Partner to whom payment is made','ECA02','The Beneficiary indicates the Business Partner to whom payment will be made.','Y','Beneficiary','Beneficiary',TO_DATE('2013-08-12 11:13:59','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
--- Aug 12, 2013 11:14:02 AM VET
--- SFAndroid Contribution
-INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Element_ID, t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Element t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Element_ID=56105 AND NOT EXISTS (SELECT * FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
-;
-
--- Aug 12, 2013 11:14:23 AM VET
--- SFAndroid Contribution
-UPDATE AD_Element_Trl SET Name='Beneficiario',PrintName='Beneficiario',Description='Socio del Negocio a quién se hace el pago',Updated=TO_DATE('2013-08-12 11:14:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=56105 AND AD_Language='es_MX'
-;
-
--- Aug 12, 2013 11:14:39 AM VET
--- SFAndroid Contribution
-UPDATE AD_Element_Trl SET Help='El beneficiario indica el socio del negocio a quién se hará el pago.',Updated=TO_DATE('2013-08-12 11:14:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=56105 AND AD_Language='es_MX'
-;
-
 -- Aug 12, 2013 11:14:59 AM VET
 -- SFAndroid Contribution
-UPDATE AD_Column SET AD_Element_ID=56105, ColumnName='Beneficiary_ID', Description='Business Partner to whom payment is made', Help='The Beneficiary indicates the Business Partner to whom payment will be made.', Name='Beneficiary',Updated=TO_DATE('2013-08-12 11:14:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66577
+UPDATE AD_Column SET AD_Element_ID=56104, ColumnName='Beneficiary_ID', Description='Business Partner to whom payment is made', Help='The Beneficiary indicates the Business Partner to whom payment will be made.', Name='Beneficiary',Updated=TO_DATE('2013-08-12 11:14:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66577
 ;
 
 -- Aug 12, 2013 11:14:59 AM VET
