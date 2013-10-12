da16442db19b83416813965cc638ea7ce8fd2e68
Yamel Senih <yamelsenih@gmail.com>
2013-10-12 13:57:50 -0530

    Fixed Error Farm Validate when Farm Division Area is equal to Farm Area

---
+++ base/org/spin/model/MFTAFact.java
@@ -0,0 +1,269 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * Copyright (C) 2003-2013 E.R.P. Consultores y Asociados, C.A.               *
+ * All Rights Reserved.                                                       *
+ * Contributor(s): Yamel Senih www.erpconsultoresyasociados.com               *
+ *****************************************************************************/
+package org.spin.model;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.Properties;
+
+import org.compiere.model.MInvoice;
+import org.compiere.model.MOrder;
+import org.compiere.util.DB;
+
+/**
+ * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a>
+ *
+ */
+public class MFTAFact extends X_FTA_Fact {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5210925643565439852L;
+
+	/**
+	 * *** Constructor ***
+	 * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a> 08/10/2013, 08:28:49
+	 * @param ctx
+	 * @param FTA_Fact_ID
+	 * @param trxName
+	 */
+	public MFTAFact(Properties ctx, int FTA_Fact_ID, String trxName) {
+		super(ctx, FTA_Fact_ID, trxName);
+	}
+
+	/**
+	 * *** Constructor ***
+	 * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a> 08/10/2013, 08:28:49
+	 * @param ctx
+	 * @param rs
+	 * @param trxName
+	 */
+	public MFTAFact(Properties ctx, ResultSet rs, String trxName) {
+		super(ctx, rs, trxName);
+	}
+	
+	/**
+	 * Delete Old Movements
+	 * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a> 10/10/2013, 14:15:21
+	 * @param p_AD_Table_ID
+	 * @param p_Record_ID
+	 * @param trxName
+	 * @return
+	 * @return int
+	 */
+	public static int deleteFact(int p_AD_Table_ID, int p_Record_ID, String trxName){
+		//	Delete Old Movements
+		StringBuffer deleteSQL = new StringBuffer ("DELETE FROM FTA_Fact ")
+			.append("WHERE AD_Table_ID = ").append(p_AD_Table_ID).append(" ")
+			.append("AND Record_ID = ").append(p_Record_ID);
+		return DB.executeUpdate(deleteSQL.toString(), trxName);
+	}
+	
+	
+	/**
+	 * Verify is Manual
+	 * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a> 10/10/2013, 14:22:21
+	 * @param p_AD_Table_ID
+	 * @param p_Record_ID
+	 * @param trxName
+	 * @return
+	 * @return boolean
+	 */
+	public static boolean isManual(int p_AD_Table_ID, int p_Record_ID, String trxName){
+		StringBuffer manualSQL = new StringBuffer ("SELECT 1 FROM FTA_Fact ")
+		.append("WHERE AD_Table_ID = ").append(p_AD_Table_ID).append(" ")
+		.append("AND Record_ID = ").append(p_Record_ID)
+		.append("AND IsManual = 'Y'");
+		//	
+		int manual = DB.getSQLValue(trxName, manualSQL.toString());
+		return (manual != 0);
+	}
+	
+	/**
+	 * Crete Fact for Order
+	 * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a> 09/10/2013, 18:04:42
+	 * @param order
+	 * @return
+	 * @return String
+	 */
+	public static String createOrderFact(Properties ctx, MOrder order, String trxName) {
+		//
+		if(isManual(MOrder.Table_ID, 
+				order.getC_Order_ID(), trxName))
+			return null;
+		
+		//	Delete Old Movements
+		deleteFact(MOrder.Table_ID, order.getC_Order_ID(), trxName);
+		
+		//	SQL
+		String sql = new String("SELECT o.AD_Org_ID, o.C_BPartner_ID, o.DateOrdered DateDoc, o.Description, " +
+				"cd.FTA_CreditDefinition_ID, cdl.FTA_CreditDefinitionLine_ID, o.FTA_FarmerCredit_ID, " +
+				"o.C_Order_ID Record_ID, ol.C_OrderLine_ID Line_ID, " +
+				"ol.LineNetAmt + (ol.LineNetAmt * t.Rate / 100) Amt " +
+				"FROM C_Order o " +
+				"INNER JOIN FTA_FarmerCredit fc ON(fc.FTA_FarmerCredit_ID = o.FTA_FarmerCredit_ID) " +
+				"INNER JOIN FTA_CreditDefinition cd ON(cd.FTA_CreditDefinition_ID = fc.FTA_CreditDefinition_ID) " +
+				"INNER JOIN C_OrderLine ol ON(ol.C_Order_ID = o.C_Order_ID) " +
+				"INNER JOIN C_Tax t ON(t.C_Tax_ID = ol.C_Tax_ID) " + 
+				"INNER JOIN FTA_CreditDefinitionLine cdl ON(cdl.FTA_CreditDefinition_ID = cd.FTA_CreditDefinition_ID) " +
+				"LEFT JOIN M_Product pr ON(pr.M_Product_ID = ol.M_Product_ID) " +
+				"WHERE o.C_Order_ID = ? " +
+				"AND (cdl.M_Product_ID = ol.M_Product_ID " +
+				"		AND ol.M_Product_ID IS NOT NULL) " +
+				"OR (cdl.M_Product_Category_ID = pr.M_Product_Category_ID " +
+				"		AND pr.M_Product_Category_ID IS NOT NULL) " +
+				"OR (cdl.C_Charge_ID = ol.C_charge_ID " +
+				"		AND ol.C_Charge_ID IS NOT NULL)");
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = DB.prepareStatement(sql, trxName);
+			//	Add Parameters
+			pstmt.setInt(1, order.getC_Order_ID());
+			//	
+			rs = pstmt.executeQuery();
+			if(rs != null){
+				while(rs.next()){
+					int m_AD_Org_ID 					= rs.getInt("AD_Org_ID");
+					int m_C_BPartner_ID 				= rs.getInt("C_BPartner_ID");
+					Timestamp m_DateDoc 				= rs.getTimestamp("DateDoc");
+					String m_Description 				= rs.getString("Description");
+					int m_FTA_CreditDefinition_ID 		= rs.getInt("FTA_CreditDefinition_ID");
+					int m_FTA_CreditDefinitionLine_ID 	= rs.getInt("FTA_CreditDefinitionLine_ID");
+					int m_FTA_FarmerCredit_ID 			= rs.getInt("FTA_FarmerCredit_ID");
+					int m_Record_ID 					= rs.getInt("Record_ID");
+					int m_Line_ID 						= rs.getInt("Line_ID");
+					BigDecimal m_Amt					= rs.getBigDecimal("Amt");
+					//	Create Fact
+					MFTAFact m_fta_Fact = new MFTAFact(ctx, 0, trxName);
+					//	Set Values
+					m_fta_Fact.setAD_Org_ID(m_AD_Org_ID);
+					m_fta_Fact.setC_BPartner_ID(m_C_BPartner_ID);
+					m_fta_Fact.setDateDoc(m_DateDoc);
+					m_fta_Fact.setDescription(m_Description);
+					m_fta_Fact.setFTA_CreditDefinition_ID(m_FTA_CreditDefinition_ID);
+					m_fta_Fact.setFTA_CreditDefinitionLine_ID(m_FTA_CreditDefinitionLine_ID);
+					m_fta_Fact.setFTA_FarmerCredit_ID(m_FTA_FarmerCredit_ID);
+					m_fta_Fact.setRecord_ID(m_Record_ID);
+					m_fta_Fact.setLine_ID(m_Line_ID);
+					m_fta_Fact.setAD_Table_ID(MOrder.Table_ID);
+					m_fta_Fact.setAmt(m_Amt);
+					//	Save
+					m_fta_Fact.saveEx();
+				}
+			}
+			//	Close DB
+			DB.close(rs, pstmt);
+		} catch (Exception e) {
+			DB.close(rs, pstmt);
+			return e.getMessage();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Create Invoice Fact
+	 * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a> 10/10/2013, 13:50:14
+	 * @param ctx
+	 * @param invoice
+	 * @param trxName
+	 * @return
+	 * @return String
+	 */
+	public static String createInvoiceFact(Properties ctx, MInvoice invoice, String trxName) {
+		//
+		if(isManual(MInvoice.Table_ID, 
+				invoice.getC_Order_ID(), trxName))
+			return null;
+		
+		//	Delete Old Movements
+		deleteFact(MInvoice.Table_ID, invoice.getC_Order_ID(), trxName);
+		
+		//	SQL
+		String sql = new String("SELECT i.AD_Org_ID, i.C_BPartner_ID, i.DateInvoiced DateDoc, i.Description, " +
+				"cd.FTA_CreditDefinition_ID, cdl.FTA_CreditDefinitionLine_ID, i.FTA_FarmerCredit_ID, " +
+				"i.C_Invoice_ID Record_ID, il.C_InvoiceLine_ID Line_ID, " +
+				"il.LineNetAmt + (il.LineNetAmt * t.Rate / 100) Amt " +
+				"FROM C_Invoice i " +
+				"INNER JOIN FTA_FarmerCredit fc ON(fc.FTA_FarmerCredit_ID = i.FTA_FarmerCredit_ID) " +
+				"INNER JOIN FTA_CreditDefinition cd ON(cd.FTA_CreditDefinition_ID = fc.FTA_CreditDefinition_ID) " +
+				"INNER JOIN C_InvoiceLine il ON(il.C_Invoice_ID = i.C_Invoice_ID) " +
+				"INNER JOIN C_Tax t ON(t.C_Tax_ID = il.C_Tax_ID) " + 
+				"INNER JOIN FTA_CreditDefinitionLine cdl ON(cdl.FTA_CreditDefinition_ID = cd.FTA_CreditDefinition_ID) " +
+				"LEFT JOIN M_Product pr ON(pr.M_Product_ID = il.M_Product_ID) " +
+				"WHERE i.C_Invoice_ID = ? " +
+				"AND (cdl.M_Product_ID = il.M_Product_ID " +
+				"		AND il.M_Product_ID IS NOT NULL) " +
+				"OR (cdl.M_Product_Category_ID = pr.M_Product_Category_ID " +
+				"		AND pr.M_Product_Category_ID IS NOT NULL) " +
+				"OR (cdl.C_Charge_ID = il.C_charge_ID " +
+				"		AND il.C_Charge_ID IS NOT NULL)");
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = DB.prepareStatement(sql, trxName);
+			//	Add Parameters
+			pstmt.setInt(1, invoice.getC_Invoice_ID());
+			//	
+			rs = pstmt.executeQuery();
+			if(rs != null){
+				while(rs.next()){
+					int m_AD_Org_ID 					= rs.getInt("AD_Org_ID");
+					int m_C_BPartner_ID 				= rs.getInt("C_BPartner_ID");
+					Timestamp m_DateDoc 				= rs.getTimestamp("DateDoc");
+					String m_Description 				= rs.getString("Description");
+					int m_FTA_CreditDefinition_ID 		= rs.getInt("FTA_CreditDefinition_ID");
+					int m_FTA_CreditDefinitionLine_ID 	= rs.getInt("FTA_CreditDefinitionLine_ID");
+					int m_FTA_FarmerCredit_ID 			= rs.getInt("FTA_FarmerCredit_ID");
+					int m_Record_ID 					= rs.getInt("Record_ID");
+					int m_Line_ID 						= rs.getInt("Line_ID");
+					BigDecimal m_Amt					= rs.getBigDecimal("Amt");
+					//	Create Fact
+					MFTAFact m_fta_Fact = new MFTAFact(ctx, 0, trxName);
+					//	Set Values
+					m_fta_Fact.setAD_Org_ID(m_AD_Org_ID);
+					m_fta_Fact.setC_BPartner_ID(m_C_BPartner_ID);
+					m_fta_Fact.setDateDoc(m_DateDoc);
+					m_fta_Fact.setDescription(m_Description);
+					m_fta_Fact.setFTA_CreditDefinition_ID(m_FTA_CreditDefinition_ID);
+					m_fta_Fact.setFTA_CreditDefinitionLine_ID(m_FTA_CreditDefinitionLine_ID);
+					m_fta_Fact.setFTA_FarmerCredit_ID(m_FTA_FarmerCredit_ID);
+					m_fta_Fact.setRecord_ID(m_Record_ID);
+					m_fta_Fact.setLine_ID(m_Line_ID);
+					m_fta_Fact.setAD_Table_ID(MInvoice.Table_ID);
+					m_fta_Fact.setAmt(m_Amt);
+					//	Save
+					m_fta_Fact.saveEx();
+				}
+			}
+			//	Close DB
+			DB.close(rs, pstmt);
+		} catch (Exception e) {
+			DB.close(rs, pstmt);
+			return e.getMessage();
+		}
+		
+		return null;
+	}
+	
+}
---
+++ base/org/spin/model/X_FTA_AllocationLine.java
@@ -0,0 +1,380 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package org.spin.model;
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.Properties;
+import org.compiere.model.*;
+import org.compiere.util.Env;
+
+/** Generated Model for FTA_AllocationLine
+ *  @author Adempiere (generated) 
+ *  @version Release 3.6.0LTS - $Id$ */
+public class X_FTA_AllocationLine extends PO implements I_FTA_AllocationLine, I_Persistent 
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 20131011L;
+
+    /** Standard Constructor */
+    public X_FTA_AllocationLine (Properties ctx, int FTA_AllocationLine_ID, String trxName)
+    {
+      super (ctx, FTA_AllocationLine_ID, trxName);
+      /** if (FTA_AllocationLine_ID == 0)
+        {
+			setAmount (Env.ZERO);
+			setC_Invoice_ID (0);
+			setDateDoc (new Timestamp( System.currentTimeMillis() ));
+			setDiscountAmt (Env.ZERO);
+			setFTA_Allocation_ID (0);
+			setFTA_AllocationLine_ID (0);
+			setWriteOffAmt (Env.ZERO);
+        } */
+    }
+
+    /** Load Constructor */
+    public X_FTA_AllocationLine (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+    /** AccessLevel
+      * @return 3 - Client - Org 
+      */
+    protected int get_AccessLevel()
+    {
+      return accessLevel.intValue();
+    }
+
+    /** Load Meta Data */
+    protected POInfo initPO (Properties ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuffer sb = new StringBuffer ("X_FTA_AllocationLine[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+
+	/** Set Allocation No.
+		@param AllocationNo Allocation No	  */
+	public void setAllocationNo (BigDecimal AllocationNo)
+	{
+		set_Value (COLUMNNAME_AllocationNo, AllocationNo);
+	}
+
+	/** Get Allocation No.
+		@return Allocation No	  */
+	public BigDecimal getAllocationNo () 
+	{
+		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_AllocationNo);
+		if (bd == null)
+			 return Env.ZERO;
+		return bd;
+	}
+
+	/** Set Amount.
+		@param Amount 
+		Amount in a defined currency
+	  */
+	public void setAmount (BigDecimal Amount)
+	{
+		set_Value (COLUMNNAME_Amount, Amount);
+	}
+
+	/** Get Amount.
+		@return Amount in a defined currency
+	  */
+	public BigDecimal getAmount () 
+	{
+		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_Amount);
+		if (bd == null)
+			 return Env.ZERO;
+		return bd;
+	}
+
+	public I_C_BPartner getC_BPartner() throws RuntimeException
+    {
+		return (I_C_BPartner)MTable.get(getCtx(), I_C_BPartner.Table_Name)
+			.getPO(getC_BPartner_ID(), get_TrxName());	}
+
+	/** Set Business Partner .
+		@param C_BPartner_ID 
+		Identifies a Business Partner
+	  */
+	public void setC_BPartner_ID (int C_BPartner_ID)
+	{
+		if (C_BPartner_ID < 1) 
+			set_Value (COLUMNNAME_C_BPartner_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_BPartner_ID, Integer.valueOf(C_BPartner_ID));
+	}
+
+	/** Get Business Partner .
+		@return Identifies a Business Partner
+	  */
+	public int getC_BPartner_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_BPartner_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	public I_C_CashLine getC_CashLine() throws RuntimeException
+    {
+		return (I_C_CashLine)MTable.get(getCtx(), I_C_CashLine.Table_Name)
+			.getPO(getC_CashLine_ID(), get_TrxName());	}
+
+	/** Set Cash Journal Line.
+		@param C_CashLine_ID 
+		Cash Journal Line
+	  */
+	public void setC_CashLine_ID (int C_CashLine_ID)
+	{
+		if (C_CashLine_ID < 1) 
+			set_Value (COLUMNNAME_C_CashLine_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_CashLine_ID, Integer.valueOf(C_CashLine_ID));
+	}
+
+	/** Get Cash Journal Line.
+		@return Cash Journal Line
+	  */
+	public int getC_CashLine_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_CashLine_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	public I_C_Invoice getC_Invoice() throws RuntimeException
+    {
+		return (I_C_Invoice)MTable.get(getCtx(), I_C_Invoice.Table_Name)
+			.getPO(getC_Invoice_ID(), get_TrxName());	}
+
+	/** Set Invoice.
+		@param C_Invoice_ID 
+		Invoice Identifier
+	  */
+	public void setC_Invoice_ID (int C_Invoice_ID)
+	{
+		if (C_Invoice_ID < 1) 
+			set_Value (COLUMNNAME_C_Invoice_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_Invoice_ID, Integer.valueOf(C_Invoice_ID));
+	}
+
+	/** Get Invoice.
+		@return Invoice Identifier
+	  */
+	public int getC_Invoice_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_Invoice_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	public I_C_Order getC_Order() throws RuntimeException
+    {
+		return (I_C_Order)MTable.get(getCtx(), I_C_Order.Table_Name)
+			.getPO(getC_Order_ID(), get_TrxName());	}
+
+	/** Set Order.
+		@param C_Order_ID 
+		Order
+	  */
+	public void setC_Order_ID (int C_Order_ID)
+	{
+		if (C_Order_ID < 1) 
+			set_Value (COLUMNNAME_C_Order_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_Order_ID, Integer.valueOf(C_Order_ID));
+	}
+
+	/** Get Order.
+		@return Order
+	  */
+	public int getC_Order_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_Order_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	public I_C_Payment getC_Payment() throws RuntimeException
+    {
+		return (I_C_Payment)MTable.get(getCtx(), I_C_Payment.Table_Name)
+			.getPO(getC_Payment_ID(), get_TrxName());	}
+
+	/** Set Payment.
+		@param C_Payment_ID 
+		Payment identifier
+	  */
+	public void setC_Payment_ID (int C_Payment_ID)
+	{
+		if (C_Payment_ID < 1) 
+			set_Value (COLUMNNAME_C_Payment_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_Payment_ID, Integer.valueOf(C_Payment_ID));
+	}
+
+	/** Get Payment.
+		@return Payment identifier
+	  */
+	public int getC_Payment_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_Payment_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Document Date.
+		@param DateDoc 
+		Date of the Document
+	  */
+	public void setDateDoc (Timestamp DateDoc)
+	{
+		set_Value (COLUMNNAME_DateDoc, DateDoc);
+	}
+
+	/** Get Document Date.
+		@return Date of the Document
+	  */
+	public Timestamp getDateDoc () 
+	{
+		return (Timestamp)get_Value(COLUMNNAME_DateDoc);
+	}
+
+	/** Set Discount Amount.
+		@param DiscountAmt 
+		Calculated amount of discount
+	  */
+	public void setDiscountAmt (BigDecimal DiscountAmt)
+	{
+		set_Value (COLUMNNAME_DiscountAmt, DiscountAmt);
+	}
+
+	/** Get Discount Amount.
+		@return Calculated amount of discount
+	  */
+	public BigDecimal getDiscountAmt () 
+	{
+		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_DiscountAmt);
+		if (bd == null)
+			 return Env.ZERO;
+		return bd;
+	}
+
+	public org.spin.model.I_FTA_Allocation getFTA_Allocation() throws RuntimeException
+    {
+		return (org.spin.model.I_FTA_Allocation)MTable.get(getCtx(), org.spin.model.I_FTA_Allocation.Table_Name)
+			.getPO(getFTA_Allocation_ID(), get_TrxName());	}
+
+	/** Set Liquidation Allocation.
+		@param FTA_Allocation_ID Liquidation Allocation	  */
+	public void setFTA_Allocation_ID (int FTA_Allocation_ID)
+	{
+		if (FTA_Allocation_ID < 1) 
+			set_Value (COLUMNNAME_FTA_Allocation_ID, null);
+		else 
+			set_Value (COLUMNNAME_FTA_Allocation_ID, Integer.valueOf(FTA_Allocation_ID));
+	}
+
+	/** Get Liquidation Allocation.
+		@return Liquidation Allocation	  */
+	public int getFTA_Allocation_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_FTA_Allocation_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Allocation Line.
+		@param FTA_AllocationLine_ID Allocation Line	  */
+	public void setFTA_AllocationLine_ID (int FTA_AllocationLine_ID)
+	{
+		if (FTA_AllocationLine_ID < 1) 
+			set_ValueNoCheck (COLUMNNAME_FTA_AllocationLine_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_FTA_AllocationLine_ID, Integer.valueOf(FTA_AllocationLine_ID));
+	}
+
+	/** Get Allocation Line.
+		@return Allocation Line	  */
+	public int getFTA_AllocationLine_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_FTA_AllocationLine_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Over/Under Payment.
+		@param OverUnderAmt 
+		Over-Payment (unallocated) or Under-Payment (partial payment) Amount
+	  */
+	public void setOverUnderAmt (BigDecimal OverUnderAmt)
+	{
+		set_Value (COLUMNNAME_OverUnderAmt, OverUnderAmt);
+	}
+
+	/** Get Over/Under Payment.
+		@return Over-Payment (unallocated) or Under-Payment (partial payment) Amount
+	  */
+	public BigDecimal getOverUnderAmt () 
+	{
+		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_OverUnderAmt);
+		if (bd == null)
+			 return Env.ZERO;
+		return bd;
+	}
+
+	/** Set Write-off Amount.
+		@param WriteOffAmt 
+		Amount to write-off
+	  */
+	public void setWriteOffAmt (BigDecimal WriteOffAmt)
+	{
+		set_Value (COLUMNNAME_WriteOffAmt, WriteOffAmt);
+	}
+
+	/** Get Write-off Amount.
+		@return Amount to write-off
+	  */
+	public BigDecimal getWriteOffAmt () 
+	{
+		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_WriteOffAmt);
+		if (bd == null)
+			 return Env.ZERO;
+		return bd;
+	}
+}
