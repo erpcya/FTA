24903dbdfe087a21592e260c637e09a5cd85e67d
Dixon Martinez <Dixon Martinez@localhost>
2013-10-15 17:33:30 -0530

    Add Report Record Weight

--- migration/postgresql/861AddViewFTA_RV_RecordWeight.sql
+++ migration/postgresql/861AddViewFTA_RV_RecordWeight.sql
@@ -6,9 +6,13 @@ rw.DocStatus, rw.DocumentNo, rw.FTA_EntryTicket_ID, rw.FTA_QualityAnalysis_ID,
 rw.FTA_RecordWeight_ID, rw.GrossWeight, rw.InDate, rw.IsSOTrx, rw.NetWeight, rw.OutDate, 
 rw.TareWeight, rw.WeightStatus, dr.FTA_Driver_ID, dr.Value, vh.FTA_Vehicle_ID, vh.VehiclePlate, 
 cp.C_BPartner_ID, cp.Value BPTaxID, cp.Name, et.FTA_MobilizationGuide_ID, qa.M_Product_ID, qa.QualityAnalysis_ID,
-oi.C_Location_ID AS Org_Location_ID, oi.TaxID
+oi.C_Location_ID AS Org_Location_ID, oi.TaxID,mg.M_Warehouse_ID, rw.FTA_RecordWeight_ID AS FTA_RV_RecordWeight_ID,
+TO_CHAR(rw.InDate,'HH:MM') InTime,
+TO_CHAR(rw.OutDate,'HH:MM') OutTime
+
 FROM FTA_RecordWeight rw
 INNER JOIN FTA_EntryTicket et ON(et.FTA_EntryTicket_ID = rw.FTA_EntryTicket_ID)
+INNER JOIN FTA_MobilizationGuide mg ON (mg.FTA_MobilizationGuide_ID = et.FTA_MobilizationGuide_ID)
 INNER JOIN FTA_Driver dr ON(dr.FTA_Driver_ID = et.FTA_Driver_ID)
 INNER JOIN FTA_Vehicle vh ON(vh.FTA_Vehicle_ID = et.FTA_Vehicle_ID)
 INNER JOIN C_BPartner cp ON(cp.C_BPartner_ID = et.C_BPartner_ID)
