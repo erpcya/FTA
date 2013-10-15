577db8e337837c47999d7ca116dc435a01d72319
Yamel Senih <Yamel Senih@localhost>
2013-10-15 11:07:57 -0530

    Add Record Weight and Quality Analysis View Add Process on Windows

--- migration/postgresql/859AddViewFTA_RV_QualityAnalisys.sql
+++ migration/postgresql/859AddViewFTA_RV_QualityAnalisys.sql
@@ -2,5 +2,7 @@
 SELECT qa.AD_Client_ID, qa.AD_Org_ID, qa.Created, qa.CreatedBy, qa.IsActive, qa.Updated, qa.UpdatedBy, 
 qa.AnalysisType, qa.C_DocType_ID, qa.DateDoc, qa.Description, 
 qa.DocStatus, qa.DocumentNo, qa.FTA_EntryTicket_ID, qa.FTA_QualityAnalysis_ID, 
-qa.IsApproved, qa.M_Product_ID, qa.Orig_QualityAnalysis_ID, qa.ProductStatus, qa.QualityAnalysis_ID
+qa.IsApproved, qa.M_Product_ID, qa.Orig_QualityAnalysis_ID, qa.ProductStatus, qa.QualityAnalysis_ID,
+oi.C_Location_ID AS Org_Location_ID, oi.TaxID
 FROM FTA_QualityAnalysis qa
+INNER JOIN AD_OrgInfo oi ON(oi.AD_Org_ID = qa.AD_Org_ID)
