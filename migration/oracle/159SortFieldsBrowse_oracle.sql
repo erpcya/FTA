6de82329fef107970faeed59ca3f5f5675bf4607
carlosaparadam <carlosaparadam@localhost>
2013-10-15 23:17:19 -0530

    Add Process Credit Allocation

---
+++ migration/oracle/159SortFieldsBrowse_oracle.sql
@@ -0,0 +1,350 @@
+-- 15/10/2013 10:24:23 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='N', SeqNo=0,Updated=TO_DATE('2013-10-15 22:24:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53339
+;
+
+-- 15/10/2013 10:24:23 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=120,Updated=TO_DATE('2013-10-15 22:24:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53315
+;
+
+-- 15/10/2013 10:24:23 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=130,Updated=TO_DATE('2013-10-15 22:24:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53316
+;
+
+-- 15/10/2013 10:24:23 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=140,Updated=TO_DATE('2013-10-15 22:24:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53322
+;
+
+-- 15/10/2013 10:24:23 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=150,Updated=TO_DATE('2013-10-15 22:24:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53331
+;
+
+-- 15/10/2013 10:24:24 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=160,Updated=TO_DATE('2013-10-15 22:24:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53332
+;
+
+-- 15/10/2013 10:24:43 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Browse_Field_Trl WHERE AD_Browse_Field_ID=53315
+;
+
+-- 15/10/2013 10:24:43 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Browse_Field WHERE AD_Browse_Field_ID=53315
+;
+
+-- 15/10/2013 10:24:44 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Browse_Field_Trl WHERE AD_Browse_Field_ID=53316
+;
+
+-- 15/10/2013 10:24:44 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Browse_Field WHERE AD_Browse_Field_ID=53316
+;
+
+-- 15/10/2013 10:24:45 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Browse_Field_Trl WHERE AD_Browse_Field_ID=53322
+;
+
+-- 15/10/2013 10:24:45 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Browse_Field WHERE AD_Browse_Field_ID=53322
+;
+
+-- 15/10/2013 10:24:46 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Browse_Field_Trl WHERE AD_Browse_Field_ID=53331
+;
+
+-- 15/10/2013 10:24:46 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Browse_Field WHERE AD_Browse_Field_ID=53331
+;
+
+-- 15/10/2013 10:24:47 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Browse_Field_Trl WHERE AD_Browse_Field_ID=53332
+;
+
+-- 15/10/2013 10:24:47 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Browse_Field WHERE AD_Browse_Field_ID=53332
+;
+
+-- 15/10/2013 10:25:22 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_View_Column WHERE AD_View_Column_ID=52148
+;
+
+-- 15/10/2013 10:25:22 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_View_Column WHERE AD_View_Column_ID=52149
+;
+
+-- 15/10/2013 10:25:23 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_View_Column WHERE AD_View_Column_ID=52155
+;
+
+-- 15/10/2013 10:25:23 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_View_Column WHERE AD_View_Column_ID=52164
+;
+
+-- 15/10/2013 10:25:24 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_View_Column WHERE AD_View_Column_ID=52165
+;
+
+-- 15/10/2013 10:25:53 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=68650
+;
+
+-- 15/10/2013 10:25:53 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=68650
+;
+
+-- 15/10/2013 10:25:55 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=68652
+;
+
+-- 15/10/2013 10:25:55 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=68652
+;
+
+-- 15/10/2013 10:25:57 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=68649
+;
+
+-- 15/10/2013 10:25:57 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=68649
+;
+
+-- 15/10/2013 10:25:58 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=68651
+;
+
+-- 15/10/2013 10:25:58 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=68651
+;
+
+-- 15/10/2013 10:26:00 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=68653
+;
+
+-- 15/10/2013 10:26:00 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=68653
+;
+
+-- 15/10/2013 10:28:32 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='N', SeqNo=0,Updated=TO_DATE('2013-10-15 22:28:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53305
+;
+
+-- 15/10/2013 10:28:32 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=90,Updated=TO_DATE('2013-10-15 22:28:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53292
+;
+
+-- 15/10/2013 10:28:32 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=100,Updated=TO_DATE('2013-10-15 22:28:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53293
+;
+
+-- 15/10/2013 10:28:32 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=110,Updated=TO_DATE('2013-10-15 22:28:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53304
+;
+
+-- 15/10/2013 10:28:32 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=120,Updated=TO_DATE('2013-10-15 22:28:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53310
+;
+
+-- 15/10/2013 10:28:32 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=130,Updated=TO_DATE('2013-10-15 22:28:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53311
+;
+
+-- 15/10/2013 10:28:42 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Browse_Field_Trl WHERE AD_Browse_Field_ID=53292
+;
+
+-- 15/10/2013 10:28:42 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Browse_Field WHERE AD_Browse_Field_ID=53292
+;
+
+-- 15/10/2013 10:28:43 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Browse_Field_Trl WHERE AD_Browse_Field_ID=53293
+;
+
+-- 15/10/2013 10:28:43 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Browse_Field WHERE AD_Browse_Field_ID=53293
+;
+
+-- 15/10/2013 10:28:43 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Browse_Field_Trl WHERE AD_Browse_Field_ID=53304
+;
+
+-- 15/10/2013 10:28:43 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Browse_Field WHERE AD_Browse_Field_ID=53304
+;
+
+-- 15/10/2013 10:28:45 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Browse_Field_Trl WHERE AD_Browse_Field_ID=53310
+;
+
+-- 15/10/2013 10:28:45 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Browse_Field WHERE AD_Browse_Field_ID=53310
+;
+
+-- 15/10/2013 10:28:45 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Browse_Field_Trl WHERE AD_Browse_Field_ID=53311
+;
+
+-- 15/10/2013 10:28:45 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Browse_Field WHERE AD_Browse_Field_ID=53311
+;
+
+-- 15/10/2013 10:29:07 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_View_Column WHERE AD_View_Column_ID=52085
+;
+
+-- 15/10/2013 10:29:08 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_View_Column WHERE AD_View_Column_ID=52086
+;
+
+-- 15/10/2013 10:29:08 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_View_Column WHERE AD_View_Column_ID=52097
+;
+
+-- 15/10/2013 10:29:09 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_View_Column WHERE AD_View_Column_ID=52103
+;
+
+-- 15/10/2013 10:29:09 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_View_Column WHERE AD_View_Column_ID=52104
+;
+
+-- 15/10/2013 10:29:34 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=68516
+;
+
+-- 15/10/2013 10:29:34 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=68516
+;
+
+-- 15/10/2013 10:29:35 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=68518
+;
+
+-- 15/10/2013 10:29:35 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=68518
+;
+
+-- 15/10/2013 10:29:36 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=68515
+;
+
+-- 15/10/2013 10:29:36 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=68515
+;
+
+-- 15/10/2013 10:29:38 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=68517
+;
+
+-- 15/10/2013 10:29:38 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=68517
+;
+
+-- 15/10/2013 10:29:39 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=68519
+;
+
+-- 15/10/2013 10:29:39 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=68519
+;
+
+-- 15/10/2013 10:31:24 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsActive='Y',Updated=TO_DATE('2013-10-15 22:31:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53288
+;
+
+-- 15/10/2013 10:33:55 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=90,Updated=TO_DATE('2013-10-15 22:33:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53305
+;
+
+-- 15/10/2013 10:35:04 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='N',Updated=TO_DATE('2013-10-15 22:35:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53305
+;
+
+-- 15/10/2013 10:44:30 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='N', IsIdentifier='Y',Updated=TO_DATE('2013-10-15 22:44:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53349
+;
+
+-- 15/10/2013 10:44:56 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='N',Updated=TO_DATE('2013-10-15 22:44:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53344
+;
+
+-- 15/10/2013 10:45:02 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='N',Updated=TO_DATE('2013-10-15 22:45:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53346
+;
+
+-- 15/10/2013 10:51:18 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='Y', SeqNo=120,Updated=TO_DATE('2013-10-15 22:51:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53339
+;
+
+-- 15/10/2013 10:51:26 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Browse_Field SET IsDisplayed='N',Updated=TO_DATE('2013-10-15 22:51:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53339
+;
+
