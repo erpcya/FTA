5a6779d92f3fce24597440e5a399cfd53308411b
Yamel Senih <yamelsenih@gmail.com>
2013-10-16 11:14:04 -0530

    Add Attribute Value List

--- migration/postgresql/857AddViewFTA_RV_AttributeValue.sql
+++ migration/postgresql/857AddViewFTA_RV_AttributeValue.sql
@@ -7,8 +7,9 @@ CASE
 	WHEN att.AttributeValueType = 'L' THEN attv.Name
 END AS AttributeValue, 
 atti.Value, atti.ValueNumber, attv.Name AttributeName,
-attsi.M_AttributeSetInstance_ID QualityAnalysis_ID
+attsi.M_AttributeSetInstance_ID QualityAnalysis_ID, attu.SeqNo
 FROM M_AttributeSetInstance attsi
 INNER JOIN M_AttributeInstance atti ON(atti.M_AttributeSetInstance_ID = attsi.M_AttributeSetInstance_ID)
 INNER JOIN M_Attribute att ON(att.M_Attribute_ID = atti.M_Attribute_ID)
+INNER JOIN M_AttributeUse attu ON(attu.M_AttributeSet_ID = attsi.M_AttributeSet_ID)
 LEFT JOIN M_AttributeValue attv ON(attv.M_AttributeValue_ID = atti.M_AttributeValue_ID)
