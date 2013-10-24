34a804ddf2fa7015d16626996793dae3fd2525a3
Dixon Martinez <Dixon Martinez@localhost>
2013-10-23 19:04:29 -0530

    Add View FTA_RV_M_Movement and FTA_RV_M_MovementLine, report Office Guide

--- migration/postgresql/850AddViewFTA_RV_LiquidationAndFTA_RV_LiquidationLine.sql
+++ migration/postgresql/850AddViewFTA_RV_LiquidationAndFTA_RV_LiquidationLine.sql
@@ -1,4 +1,5 @@
-﻿CREATE OR REPLACE VIEW FTA_RV_Liquidation AS
+﻿-- DROP VIEW FTA_RV_Liquidation ;
+CREATE OR REPLACE VIEW FTA_RV_Liquidation AS
 SELECT 	
 	-- Documento Liquidación
 	fl.FTA_FarmerLiquidation_ID,  
@@ -9,12 +10,13 @@ SELECT
 	fl.UpdatedBy,
 	fl.Created,  
 	fl.CreatedBy,
+	fl.IsActive,
+	
 	fl.Processed,  
-	fl.DateDoc,  
-	fl.DocAction,  
+	fl.DateDoc,    
 	fl.DocStatus,  
 	fl.DocumentNo,  
-	fl.IsActive,	   
+		   
 	fl.Amt,    
 	NetWeight, 
 	--Producto
@@ -23,7 +25,7 @@ SELECT
 	--Socio del Negocio
 	fl.C_BPartner_ID,
 	COALESCE(bp.Value,bp.TaxID) AS BPTaxID,
-	bp.Name || bp.Name2 AS Name,
+	bp.Name || COALESCE(bp.Name2,'') AS Name,
 	--Tipo de Documento
 	fl.C_DocType_ID,  
 	dt.PrintName AS DocumentType,
@@ -36,17 +38,22 @@ SELECT
 
 	-- Crédito Productor
 	fl.FTA_FarmerCredit_ID,
-	fl.FTA_FarmerLiquidation_ID AS FTA_RV_Liquidation_ID
+	fl.FTA_FarmerLiquidation_ID AS FTA_RV_Liquidation_ID,
+	fl.FTA_CategoryCalc_ID,
+	fl.C_Currency_ID
+
+	
 FROM FTA_FarmerLiquidation fl
 INNER JOIN C_DocType dt ON (dt.C_DocType_ID = fl.C_DocType_ID)
 INNER JOIN C_BPartner bp ON (bp.C_BPartner_ID = fl.C_BPartner_ID)
 INNER JOIN M_Product p ON (p.M_Product_ID = fl.M_Product_ID)
-LEFT JOIN FTA_FarmerCredit fc ON (fc.FTA_FarmerCredit_ID = fl.FTA_FarmerCredit_ID)
+INNER JOIN FTA_FarmerCredit fc ON (fc.FTA_FarmerCredit_ID = fl.FTA_FarmerCredit_ID)
 INNER JOIN AD_OrgInfo  oi ON (oi.AD_Org_ID = fl.AD_Org_ID)
 LEFT JOIN C_Invoice i ON (i.C_Invoice_ID = fl.C_Invoice_ID)
+LEFT JOIN FTA_CategoryCalc cc ON (cc.FTA_CategoryCalc_ID = fl.FTA_CategoryCalc_ID)
 ;
 
-
+--DROP VIEW FTA_RV_LiquidationLine ;
 CREATE OR REPLACE VIEW FTA_RV_LiquidationLine AS
 SELECT 
 	FTA_FarmerLiquidationLine_ID,  
@@ -63,19 +70,10 @@ SELECT
 	Price,  
 	PriceList,
 	
-	fll.FTA_FarmerLiquidation_ID,  
+	fll.FTA_FarmerLiquidation_ID,  	
+	fll.FTA_RecordWeight_ID 
 	
-	fll.FTA_RecordWeight_ID, 
-
-	--Almacen
-	io.M_InOut_ID,
-	io.M_WareHouse_ID,
-	M_Locator_ID	
 FROM FTA_FarmerLiquidationLine fll
 INNER JOIN FTA_FarmerLiquidation fl ON (fll.FTA_FarmerLiquidation_ID =fl.FTA_FarmerLiquidation_ID)
-INNER JOIN FTA_CategoryCalc cc ON (cc.FTA_CategoryCalc_ID = fl.FTA_CategoryCalc_ID)
 INNER JOIN FTA_RecordWeight rw ON (rw.FTA_RecordWeight_ID = fll.FTA_RecordWeight_ID)
-LEFT JOIN M_InOut io ON (io.FTA_RecordWeight_ID= rw.FTA_RecordWeight_ID)
-LEFT JOIN M_WareHouse wh ON (wh.M_WareHouse_ID  = io.M_WareHouse_ID)
-LEFT JOIN M_Locator l ON (l.M_WareHouse_ID = wh.M_WareHouse_ID)
 ;
