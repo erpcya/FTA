e2e0a8c0e09a4a8a2ca6d61dc27a23bd943f5d69
Carlos Parada <carlosaparadam@gmail.com>
2013-10-24 11:11:01 -0530

    Add Category DFL In Browse

---
+++ migration/postgresql/858AddViewFTA_RV_MovilizationGuide.sql
@@ -0,0 +1,55 @@
+﻿--DROP VIEW FTA_RV_MobilizationGuide;
+CREATE VIEW FTA_RV_MobilizationGuide AS 
+SELECT  
+	mg.AD_Client_ID, --Compañia 
+	mg.AD_Org_ID, -- Organizacion
+	oi.C_Location_ID AS Org_Location_ID, --Direccion Org
+	oi.TaxID, --RIF Org
+	mg.C_BPartner_ID, -- BPartner
+	COALESCE(bp.TaxID,bp.Value) AS BPTaxID, --RIF BPartner
+	bp.Name AS BPName,
+	bp.Name2 AS BPName2,
+	bpl.C_BPartner_Location_ID,
+	mg.C_DocType_ID,
+	mg.Description,
+	mg.DocStatus,
+	mg.DocAction, 
+	mg.DocumentNo,
+	mg.FTA_Farming_ID,
+	mg.FTA_MobilizationGuide_ID,
+	mg.IsActive,
+	mg.Processed,
+	
+	mg.ValidTo,
+	mg.M_Warehouse_ID,
+	ci.C_Uom_Weight_ID, -- Unidad de medida para el peso
+	mg.FTA_VehicleType_ID,
+	vt.Name AS VehicleType,
+	ROUND(vt.LoadCapacity ,2)||' '|| u.Uomsymbol AS ChargeCapacity,
+	l.Address1||' '||l.Address2||' '||l.Address3||' '||l.Address4 AS Address,
+	l.C_Region_ID,
+	l.C_Country_ID,
+	l.C_City_ID,
+	fa.Name AS FarmName,
+	
+	p.Value,
+	p.Name,
+	p.C_Uom_ID,
+	mg.QtyToDeliver,
+	ROUND(mg.QtyToDeliver,2) AS Tonne,
+	mg.FTA_MobilizationGuide_ID AS FTA_RV_MobilizationGuide_ID,
+	mg.DateDoc
+	
+FROM FTA_MobilizationGuide mg
+INNER JOIN FTA_Farming f ON (f.FTA_Farming_ID = mg.FTA_Farming_ID)
+INNER JOIN M_Product p ON (f.Category_ID = p.M_Product_ID)
+INNER JOIN FTA_FarmDivision fd ON (fd.FTA_FarmDivision_ID = f.FTA_FarmDivision_ID)
+INNER JOIN FTA_Farm fa ON (fa.FTA_Farm_ID = fd.FTA_Farm_ID)
+INNER JOIN C_BPartner bp ON (bp.C_BPartner_ID = fa.C_BPartner_ID)
+LEFT JOIN c_bpartner_location bpl ON (bpl.C_BPartner_ID = bp.C_BPartner_ID)
+LEFT JOIN C_Location l ON (l.C_Location_ID = bpl.C_Location_ID)
+LEFT JOIN FTA_VehicleType vt ON (vt.FTA_VehicleType_ID = mg.FTA_VehicleType_ID)
+LEFT JOIN M_Warehouse w ON (w.M_Warehouse_ID = mg.M_Warehouse_ID)
+LEFT JOIN AD_OrgInfo oi ON (oi.AD_Org_ID = mg.AD_Org_ID)
+LEFT JOIN AD_ClientInfo ci ON (ci.AD_Client_ID = mg.AD_Client_ID)
+LEFT JOIN C_uom u ON (u.C_Uom_ID = ci.C_Uom_Weight_ID)
---
+++ migration/postgresql/864AddFunction_liquidationAvailable.sql
@@ -0,0 +1,59 @@
+﻿-- Function: liquidationAvailable(numeric)
+
+-- DROP FUNCTION liquidationAvailable(numeric);
+
+CREATE OR REPLACE FUNCTION liquidationAvailable(p_FTA_FarmerLiquidation_id numeric)
+  RETURNS numeric AS
+$BODY$
+/*************************************************************************
+ * The contents of this file are subject to the Compiere License.  You may
+ * obtain a copy of the License at    http://www.compiere.org/license.html
+ * Software is on an  "AS IS" basis,  WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the License for details. Code: Compiere ERP+CRM
+ * Copyright (C) 1999-2001 Jorg Janke, ComPiere, Inc. All Rights Reserved.
+ *
+ * converted to postgreSQL by Karsten Thiemann (Schaeffer AG), 
+ * kthiemann@adempiere.org
+ *************************************************************************
+ * Title:	Calculate Available Payment Amount in Payment Currency
+ * Description:
+ *		similar to C_Invoice_Open
+ ************************************************************************/
+DECLARE
+	v_Currency_ID		NUMERIC(10);
+	v_AvailableAmt		NUMERIC := 0;
+    	v_Amt               	NUMERIC := 0;
+    	r   			RECORD;
+
+BEGIN
+    --	Get Currency
+	SELECT	C_Currency_ID, Amt
+	  INTO	v_Currency_ID, v_AvailableAmt
+	FROM	FTA_FarmerLiquidation     -- corrected for AP/AR
+	WHERE	FTA_FarmerLiquidation_ID = p_FTA_FarmerLiquidation_ID;
+--  DBMS_OUTPUT.PUT_LINE('== FTA_FarmerLiquidation_ID=' || p_FTA_FarmerLiquidation_ID || ', Amt=' || v_AvailableAmt);
+
+	--	Calculate Allocated Amount
+	FOR r IN
+		SELECT	a.AD_Client_ID, a.AD_Org_ID, al.Amount, a.C_Currency_ID, a.DateDoc
+		FROM	FTA_AllocationLine al
+	        INNER JOIN FTA_Allocation a ON (al.FTA_Allocation_ID=a.FTA_Allocation_ID)
+		WHERE	al.FTA_FarmerLiquidation_ID = p_FTA_FarmerLiquidation_ID
+          	AND   a.IsActive='Y'
+	LOOP
+        v_Amt := currencyConvert(r.Amount, r.C_Currency_ID, v_Currency_ID, r.DateDoc, null, r.AD_Client_ID, r.AD_Org_ID);
+	    v_AvailableAmt := v_AvailableAmt - v_Amt;
+--      DBMS_OUTPUT.PUT_LINE('  Allocation=' || a.Amount || ' - Available=' || v_AvailableAmt);
+	END LOOP;
+	--	Ignore Rounding
+	IF (v_AvailableAmt BETWEEN -0.00999 AND 0.00999) THEN
+		v_AvailableAmt := 0;
+	END IF;
+	--	Round to penny
+	v_AvailableAmt := ROUND(COALESCE(v_AvailableAmt,0), 2);
+	RETURN	v_AvailableAmt;
+END;
+
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
