b36feb431a48217aa5fceed4f6d6160dd1b4b446
Yamel Senih <Yamel Senih@localhost>
2013-10-29 10:41:59 -0530

    Add Table Entry Ticket Guide and Column

--- base/org/spin/process/FarmerCreditBEGenerate.java
+++ base/org/spin/process/FarmerCreditBEGenerate.java
@@ -16,12 +16,14 @@
  *****************************************************************************/
 package org.spin.process;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.AdempiereUserError;
 import org.compiere.util.DB;
+import org.compiere.util.Env;
 import org.spin.model.MFTABillOfExchange;
 import org.spin.model.MFTACreditAct;
 import org.spin.model.MFTAFarmerCredit;
@@ -47,8 +49,11 @@ public class FarmerCreditBEGenerate extends SvrProcess {
 	private Timestamp	p_ValidTo = null;
 	/**	Farmer Credit			*/
 	private int 		p_FTA_FarmerCredit_ID = 0;
+	/**	Quantity				*/
+	private int 		p_Qty = 0;
 	/**	Created					*/
 	private int 		m_Created = 0;
+	
 	@Override
 	protected void prepare() {
 		for (ProcessInfoParameter para:getParameter()){
@@ -68,6 +73,8 @@ public class FarmerCreditBEGenerate extends SvrProcess {
 				p_DateDoc = (Timestamp)para.getParameter();
 			else if (name.equals("ValidTo"))
 				p_ValidTo = (Timestamp)para.getParameter();
+			else if(name.equals("Qty"))
+				p_Qty = para.getParameterAsInt();
 		}
 		//	Get Technical From Identifier
 		if(p_FTA_FarmerCredit_ID == 0
@@ -92,6 +99,9 @@ public class FarmerCreditBEGenerate extends SvrProcess {
 		//	Valid Document Date
 		if(p_DateDoc == null)
 			throw new AdempiereUserError("@DateDoc@ @NotFount@");
+		//	Valid Quantity
+		if(p_Qty <= 0)		
+			throw new AdempiereUserError("@Qty@ <= @0@");
 		
 		if(p_FTA_FarmerCredit_ID != 0){
 			MFTAFarmerCredit m_FTA_FarmerCredit = new MFTAFarmerCredit(getCtx(), p_FTA_FarmerCredit_ID, get_TrxName());
@@ -123,43 +133,51 @@ public class FarmerCreditBEGenerate extends SvrProcess {
 		//	
 		if(billOfExchange_ID != 0)
 			return;
-		
-		MFTABillOfExchange m_FTA_BillOfExchange = new MFTABillOfExchange(getCtx(), 0, get_TrxName());
-		//	Organization
-		if(p_AD_Org_ID == 0)
-			p_AD_Org_ID = p_FTA_FarmerCredit.getAD_Org_ID();
-		
-		m_FTA_BillOfExchange.setC_DocType_ID(p_C_DocTypeTarget_ID);
-		m_FTA_BillOfExchange.setDateDoc(p_DateDoc);
-		m_FTA_BillOfExchange.setC_BPartner_ID(p_FTA_FarmerCredit.getC_BPartner_ID());
-		//	Change to Approved Amount
-		m_FTA_BillOfExchange.setAmt(p_FTA_FarmerCredit.getAmt());
-		m_FTA_BillOfExchange.setFTA_FarmerCredit_ID(p_FTA_FarmerCredit.getFTA_FarmerCredit_ID());
-		if(p_C_BPartner_Location_ID == 0)
-			p_C_BPartner_Location_ID = DB.getSQLValue(get_TrxName(), "SELECT MAX(fr.C_BPartner_Location_ID) " +
-					"FROM FTA_Farm fr " +
-					"INNER JOIN FTA_FarmDivision fd ON(fd.FTA_Farm_ID = fr.FTA_Farm_ID) " +
-					"INNER JOIN FTA_Farming fm ON(fm.FTA_FarmDivision_ID = fd.FTA_FarmDivision_ID) " +
-					"WHERE fm.FTA_FarmerCredit_ID = ?", p_FTA_FarmerCredit.getFTA_FarmerCredit_ID());
-		
-		m_FTA_BillOfExchange.setC_BPartner_Location_ID(p_C_BPartner_Location_ID);
-		if(p_ValidTo != null)
-			m_FTA_BillOfExchange.setValidTo(p_ValidTo);
-		
-		m_FTA_BillOfExchange.setStatus(X_FTA_BillOfExchange.STATUS_Generated);
+		//	Set Amount
+		BigDecimal approvedAmt = p_FTA_FarmerCredit.getApprovedAmt();
+		if(approvedAmt == null
+				|| approvedAmt.equals(Env.ZERO))
+			return;
+		BigDecimal amt = approvedAmt.divide(new BigDecimal(p_Qty));
 		//	
-		m_FTA_BillOfExchange.saveEx();
-		
-		//	Process Bill of Exchange
-		m_FTA_BillOfExchange.setDocAction(X_FTA_BillOfExchange.DOCACTION_Complete);
-		m_FTA_BillOfExchange.processIt(X_FTA_BillOfExchange.DOCACTION_Complete);
-		m_FTA_BillOfExchange.saveEx();
-		
-		addLog("@FTA_BillOfExchange_ID@: " + m_FTA_BillOfExchange.getDocumentNo() + 
-				" - @Amt@ = " + m_FTA_BillOfExchange.getAmt());
-		//	
-		m_Created ++;
-
+		for(int i = 0; i < p_Qty; i ++){
+			//	
+			MFTABillOfExchange m_FTA_BillOfExchange = new MFTABillOfExchange(getCtx(), 0, get_TrxName());
+			//	Organization
+			if(p_AD_Org_ID == 0)
+				p_AD_Org_ID = p_FTA_FarmerCredit.getAD_Org_ID();
+			
+			m_FTA_BillOfExchange.setC_DocType_ID(p_C_DocTypeTarget_ID);
+			m_FTA_BillOfExchange.setDateDoc(p_DateDoc);
+			m_FTA_BillOfExchange.setC_BPartner_ID(p_FTA_FarmerCredit.getC_BPartner_ID());
+			//	
+			m_FTA_BillOfExchange.setAmt(amt);
+			m_FTA_BillOfExchange.setFTA_FarmerCredit_ID(p_FTA_FarmerCredit.getFTA_FarmerCredit_ID());
+			if(p_C_BPartner_Location_ID == 0)
+				p_C_BPartner_Location_ID = DB.getSQLValue(get_TrxName(), "SELECT MAX(fr.C_BPartner_Location_ID) " +
+						"FROM FTA_Farm fr " +
+						"INNER JOIN FTA_FarmDivision fd ON(fd.FTA_Farm_ID = fr.FTA_Farm_ID) " +
+						"INNER JOIN FTA_Farming fm ON(fm.FTA_FarmDivision_ID = fd.FTA_FarmDivision_ID) " +
+						"WHERE fm.FTA_FarmerCredit_ID = ?", p_FTA_FarmerCredit.getFTA_FarmerCredit_ID());
+			
+			m_FTA_BillOfExchange.setC_BPartner_Location_ID(p_C_BPartner_Location_ID);
+			if(p_ValidTo != null)
+				m_FTA_BillOfExchange.setValidTo(p_ValidTo);
+			
+			m_FTA_BillOfExchange.setStatus(X_FTA_BillOfExchange.STATUS_Generated);
+			//	
+			m_FTA_BillOfExchange.saveEx();
+			
+			//	Process Bill of Exchange
+			m_FTA_BillOfExchange.setDocAction(X_FTA_BillOfExchange.DOCACTION_Complete);
+			m_FTA_BillOfExchange.processIt(X_FTA_BillOfExchange.DOCACTION_Complete);
+			m_FTA_BillOfExchange.saveEx();
+			
+			addLog("@FTA_BillOfExchange_ID@: " + m_FTA_BillOfExchange.getDocumentNo() + 
+					" - @Amt@ = " + m_FTA_BillOfExchange.getAmt());
+			//	
+			m_Created ++;
+		}
 	}
 	
 
