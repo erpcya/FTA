e6ca4f4c01b7b88d826d7371c1501e542a6ea599
Yamel Senih <yamelsenih@gmail.com>
2013-10-31 18:41:01 -0530

    Add Calculate Interest

--- migration/postgresql/259AddColumnInTableC_InvoiceLineandElementDocAffected.sql
+++ migration/postgresql/259AddColumnInTableC_InvoiceLineandElementDocAffected.sql
@@ -1,13 +1,3 @@
--- Jul 30, 2013 10:15:11 AM VET
--- LVE Withholding
-INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,EntityType,FieldLength,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,66489,56063,0,19,333,'LVE_WithholdingConfig_ID',TO_TIMESTAMP('2013-07-30 10:15:11','YYYY-MM-DD HH24:MI:SS'),100,'ECA02',10,'Y','Y','N','N','N','N','N','N','N','N','N','N','Y','Withholding Config',0,TO_TIMESTAMP('2013-07-30 10:15:11','YYYY-MM-DD HH24:MI:SS'),100,0)
-;
-
--- Jul 30, 2013 10:15:11 AM VET
--- LVE Withholding
-INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=66489 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
-;
-
 -- Jul 30, 2013 10:17:46 AM VET
 -- LVE Withholding
 INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,56067,0,'DocAffected',TO_TIMESTAMP('2013-07-30 10:17:46','YYYY-MM-DD HH24:MI:SS'),100,'ECA02','Y','Doc Affected','Doc Affected',TO_TIMESTAMP('2013-07-30 10:17:46','YYYY-MM-DD HH24:MI:SS'),100)
@@ -32,4 +22,3 @@ INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Refe
 -- LVE Withholding
 INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=66490 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
 ;
-
