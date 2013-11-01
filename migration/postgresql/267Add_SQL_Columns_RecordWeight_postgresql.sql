dcde5e5c625f7686be7da7606daf8fa48511dd6f
Yamel Senih <Yamel Senih@localhost>
2013-11-01 16:01:10 -0530

    Add Search Columns on Record Weight

---
+++ migration/postgresql/267Add_SQL_Columns_RecordWeight_postgresql.sql
@@ -0,0 +1,190 @@
+-- Nov 1, 2013 4:31:58 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET Callout=NULL,Updated=TO_TIMESTAMP('2013-11-01 16:31:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66903
+;
+
+-- Nov 1, 2013 4:36:44 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,ColumnSQL,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,69653,455,0,19,53561,'M_Shipper_ID','(SELECT et.M_Shipper_ID FROM FTA_EntryTicket et WHERE et.FTA_EntryTicket_ID = FTA_RecordWeight.FTA_EntryTicket_ID)',TO_TIMESTAMP('2013-11-01 16:36:42','YYYY-MM-DD HH24:MI:SS'),100,'Method or manner of product delivery','ECA02',22,'The Shipper indicates the method of delivering product','Y','Y','N','N','N','N','N','N','N','Y','N','N','N','Shipper',0,TO_TIMESTAMP('2013-11-01 16:36:42','YYYY-MM-DD HH24:MI:SS'),100,0)
+;
+
+-- Nov 1, 2013 4:36:44 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=69653 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+-- Nov 1, 2013 4:37:55 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Reference_Value_ID,AD_Table_ID,ColumnName,ColumnSQL,Created,CreatedBy,EntityType,FieldLength,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,69654,56441,0,30,53567,53561,'FTA_Driver_ID','(SELECT et.FTA_Driver_ID FROM FTA_EntryTicket et WHERE et.FTA_EntryTicket_ID = FTA_RecordWeight.FTA_EntryTicket_ID)',TO_TIMESTAMP('2013-11-01 16:37:53','YYYY-MM-DD HH24:MI:SS'),100,'ECA02',10,'Y','Y','N','N','N','N','N','N','N','N','N','N','N','Driver',0,TO_TIMESTAMP('2013-11-01 16:37:53','YYYY-MM-DD HH24:MI:SS'),100,0)
+;
+
+-- Nov 1, 2013 4:37:55 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=69654 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+-- Nov 1, 2013 4:37:57 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2013-11-01 16:37:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=69654
+;
+
+-- Nov 1, 2013 4:39:00 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Reference_Value_ID,AD_Table_ID,ColumnName,ColumnSQL,Created,CreatedBy,EntityType,FieldLength,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,69655,56440,0,30,53568,53561,'FTA_Vehicle_ID','(SELECT et.FTA_Vehicle_ID FROM FTA_EntryTicket et WHERE et.FTA_EntryTicket_ID = FTA_RecordWeight.FTA_EntryTicket_ID)',TO_TIMESTAMP('2013-11-01 16:38:58','YYYY-MM-DD HH24:MI:SS'),100,'ECA02',10,'Y','Y','N','N','N','N','N','N','N','Y','N','N','N','Vehicle',0,TO_TIMESTAMP('2013-11-01 16:38:58','YYYY-MM-DD HH24:MI:SS'),100,0)
+;
+
+-- Nov 1, 2013 4:39:00 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=69655 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+-- Nov 1, 2013 4:41:19 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET IsSelectionColumn='N', IsUpdateable='N',Updated=TO_TIMESTAMP('2013-11-01 16:41:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66915
+;
+
+-- Nov 1, 2013 4:44:28 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Reference_Value_ID,AD_Table_ID,ColumnName,ColumnSQL,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,69656,540,0,30,53537,53561,'ReferenceNo','(FTA_RecordWeight.FTA_EntryTicket_ID)',TO_TIMESTAMP('2013-11-01 16:44:26','YYYY-MM-DD HH24:MI:SS'),100,'Your customer or vendor number at the Business Partner''s site','ECA02',40,'The reference number can be printed on orders and invoices to allow your business partner to faster identify your records.','Y','Y','N','N','N','N','N','N','N','Y','N','N','N','Reference No',0,TO_TIMESTAMP('2013-11-01 16:44:26','YYYY-MM-DD HH24:MI:SS'),100,0)
+;
+
+-- Nov 1, 2013 4:44:28 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=69656 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+-- Nov 1, 2013 4:45:01 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,69654,70254,0,53731,TO_TIMESTAMP('2013-11-01 16:44:59','YYYY-MM-DD HH24:MI:SS'),100,10,'ECA02','Y','Y','Y','N','N','N','N','N','Driver',TO_TIMESTAMP('2013-11-01 16:44:59','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Nov 1, 2013 4:45:01 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=70254 AND NOT EXISTS (SELECT * FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- Nov 1, 2013 4:45:03 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,69656,70255,0,53731,TO_TIMESTAMP('2013-11-01 16:45:01','YYYY-MM-DD HH24:MI:SS'),100,'Your customer or vendor number at the Business Partner''s site',40,'ECA02','The reference number can be printed on orders and invoices to allow your business partner to faster identify your records.','Y','Y','Y','N','N','N','N','N','Reference No',TO_TIMESTAMP('2013-11-01 16:45:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Nov 1, 2013 4:45:03 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=70255 AND NOT EXISTS (SELECT * FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- Nov 1, 2013 4:45:05 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,69653,70256,0,53731,TO_TIMESTAMP('2013-11-01 16:45:03','YYYY-MM-DD HH24:MI:SS'),100,'Method or manner of product delivery',22,'ECA02','The Shipper indicates the method of delivering product','Y','Y','Y','N','N','N','N','N','Shipper',TO_TIMESTAMP('2013-11-01 16:45:03','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Nov 1, 2013 4:45:05 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=70256 AND NOT EXISTS (SELECT * FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- Nov 1, 2013 4:45:07 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,69655,70257,0,53731,TO_TIMESTAMP('2013-11-01 16:45:05','YYYY-MM-DD HH24:MI:SS'),100,10,'ECA02','Y','Y','Y','N','N','N','N','N','Vehicle',TO_TIMESTAMP('2013-11-01 16:45:05','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Nov 1, 2013 4:45:07 PM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=70257 AND NOT EXISTS (SELECT * FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- Nov 1, 2013 4:45:37 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=260,Updated=TO_TIMESTAMP('2013-11-01 16:45:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70193
+;
+
+-- Nov 1, 2013 4:45:38 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=270,Updated=TO_TIMESTAMP('2013-11-01 16:45:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70215
+;
+
+-- Nov 1, 2013 4:45:38 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=280,Updated=TO_TIMESTAMP('2013-11-01 16:45:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70255
+;
+
+-- Nov 1, 2013 4:45:38 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=290,Updated=TO_TIMESTAMP('2013-11-01 16:45:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70256
+;
+
+-- Nov 1, 2013 4:45:38 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=300,Updated=TO_TIMESTAMP('2013-11-01 16:45:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70254
+;
+
+-- Nov 1, 2013 4:45:38 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=310,Updated=TO_TIMESTAMP('2013-11-01 16:45:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70257
+;
+
+-- Nov 1, 2013 4:45:48 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2013-11-01 16:45:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70256
+;
+
+-- Nov 1, 2013 4:45:52 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2013-11-01 16:45:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70257
+;
+
+-- Nov 1, 2013 4:46:04 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET AD_FieldGroup_ID=104,Updated=TO_TIMESTAMP('2013-11-01 16:46:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70255
+;
+
+-- Nov 1, 2013 4:47:55 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET DisplayLength=10,Updated=TO_TIMESTAMP('2013-11-01 16:47:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70255
+;
+
+-- Nov 1, 2013 4:48:02 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET DisplayLength=10,Updated=TO_TIMESTAMP('2013-11-01 16:48:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70256
+;
+
+-- Nov 1, 2013 4:50:18 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Ref_Table SET AD_Display=68499,Updated=TO_TIMESTAMP('2013-11-01 16:50:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=53568
+;
+
+-- Nov 1, 2013 4:50:36 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Ref_Table SET IsValueDisplayed='Y',Updated=TO_TIMESTAMP('2013-11-01 16:50:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=53568
+;
+
+-- Nov 1, 2013 4:50:46 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Ref_Table SET IsValueDisplayed='N',Updated=TO_TIMESTAMP('2013-11-01 16:50:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=53568
+;
+
+-- Nov 1, 2013 4:53:39 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET AD_Reference_ID=19, AD_Reference_Value_ID=NULL,Updated=TO_TIMESTAMP('2013-11-01 16:53:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=69655
+;
+
+-- Nov 1, 2013 4:55:01 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET AD_Reference_ID=18,Updated=TO_TIMESTAMP('2013-11-01 16:55:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=69654
+;
+
+-- Nov 1, 2013 4:55:28 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET IsCentrallyMaintained='N',Updated=TO_TIMESTAMP('2013-11-01 16:55:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70255
+;
+
+-- Nov 1, 2013 4:55:49 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field_Trl SET Name='Ticket de Entrada',Description='Ticket de Entrada',Help='Ticket de Entrada',Updated=TO_TIMESTAMP('2013-11-01 16:55:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70255 AND AD_Language='es_MX'
+;
+
+-- Nov 1, 2013 4:56:04 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field_Trl SET Name='Ticket de Entrada',Description='Ticket de Entrada',Help='Ticket de Entrada',Updated=TO_TIMESTAMP('2013-11-01 16:56:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70255 AND AD_Language='es_VE'
+;
+
