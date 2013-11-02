030d33c33968f858f55fb65871910d0eda622c64
Dixon Martinez <dixon.22martinez@gmail.com>
2013-11-02 15:07:13 -0530

    Update view FTA_RV_MobilizationGuide and add column C_UOM_Area_ID Into AD_ClientInfo

---
+++ migration/oracle/181UpdateValidationParameterCreditActIntoProcessBillOfExChangeDetail.sql
@@ -0,0 +1,5 @@
+-- 22/10/2013 07:37:32 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Process_Para SET AD_Reference_ID=18, AD_Reference_Value_ID=53576,Updated=TO_DATE('2013-10-22 19:37:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=54184
+;
+
