89a72f594e4d7bde2d50f00fee30a687cbea99d5
Carlos Parada <carlosaparadam@gmail.com>
2013-11-03 18:05:40 -0530

    Add Fields Mandatory Browse

---
+++ migration/oracle/282AddFieldsMandatoryForBrowseGenerateInvoice_oracle.sql
@@ -0,0 +1,5 @@
+-- 03/11/2013 07:00:39 PM VET
+-- CGI Anca
+UPDATE AD_Browse_Field SET IsMandatory='Y',Updated=TO_DATE('2013-11-03 19:00:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=1013262 WHERE AD_Browse_Field_ID=53435
+;
+
