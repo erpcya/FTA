5f0477492fccc21979537d395ba53ddfef808863
Carlos Parada <carlosaparadam@gmail.com>
2013-11-03 16:41:54 -0530

    Add Browse Generate Invoice From Liquidation

---
+++ migration/postgresql/882AddViewFTA_RV_LiquidationsForInvoice.sql
@@ -0,0 +1,27 @@
+﻿--DROP VIEW FTA_RV_LiquidationsForInvoice;
+Create Or Replace View FTA_RV_LiquidationsForInvoice AS
+Select 
+fl.AD_Client_ID,
+fl.AD_Org_ID,
+fl.Amt,
+fl.C_BPartner_ID,
+fl.C_ConversionType_ID,
+fl.C_Currency_ID,
+fl.C_DocType_ID,
+fl.C_Invoice_ID,
+fl.DateDoc,
+fl.Description,
+fl.DocAction,
+fl.DocStatus,
+fl.DocumentNo,
+fl.FTA_CategoryCalc_ID,
+fl.FTA_FarmerCredit_ID,
+fl.FTA_FarmerLiquidation_ID,
+fl.IsActive,
+fl.IsApproved,
+fl.M_Product_ID,
+fl.NetWeight,
+fl.Processed,
+LiquidationAvailable(fl.FTA_FarmerLiquidation_ID) AvailableAmt
+From 
+FTA_FarmerLiquidation fl;
