284f5243e246c5ac746d6ffa41067673cbc811bd
Dixon Martinez <dixon.22martinez@gmail.com>
2013-11-09 11:18:25 -0530

    Update FTA_CreditDefinition_ID Into Table FTA_FarmerCredit

---
+++ migration/postgresql/291UpdateFTA_CreditDefinition_IDIntoTableFTA_FarmerCredit.sql
@@ -0,0 +1,5 @@
+-- Nov 9, 2013 12:12:55 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsUpdateable='Y',Updated=TO_TIMESTAMP('2013-11-09 12:12:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66694
+;
+
