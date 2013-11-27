0f3712d718a40aa826f3986c58b86c7e8f3e4ee3
carlosaparadam <carlosaparadam@localhost>
2013-11-27 09:49:13 -0530

    Correct Browse Credit Act

--- migration/postgresql/853AddViewFTA_RV_AllocationCreditAct.sql
+++ migration/postgresql/853AddViewFTA_RV_AllocationCreditAct.sql
@@ -27,8 +27,7 @@ CASE WHEN fc.FTA_CreditAct_ID IS NULL THEN 'N' ELSE 'Y' END AS IsAllocated,
 CASE WHEN fc.ApprovedQty= 0 THEN fc.Qty ELSE fc.ApprovedQty END AS ApprovedQty,
 CASE WHEN fc.ApprovedAmt= 0 THEN fc.Amt ELSE fc.ApprovedAmt END AS ApprovedAmt
 FROM FTA_FarmerCredit fc 
-WHERE fc.DocStatus IN('DR','IP') 
-AND (
+(
   (Exists (Select 1 From FTA_Farming fming Where fc.FTA_FarmerCredit_ID=fming.FTA_FarmerCredit_ID) And fc.CreditType<>'L') 
   Or fc.CreditType='L')
 ;
