189b2b24d18ddf8355066a740c7b769b532979d0
dixon22ma <dixon.22martinez@gmail.com>
2013-12-05 14:42:28 -0530

    Update view FTA_RV_M_Movement and FTA_RV_M_MovementLine

--- migration/postgresql/868AddViewFTA_RV_M_Movement.sql
+++ migration/postgresql/868AddViewFTA_RV_M_Movement.sql
@@ -12,8 +12,7 @@ SELECT
 	m.AD_User_ID, 
 	m.ApprovalAmt, 
 	m.C_Activity_ID, 
-	m.C_BPartner_ID, 
-	m.C_BPartner_Location_ID, 
+	m.C_BPartner_ID,
 	m.C_Campaign_ID, 
 	m.C_Charge_ID, 
 	m.C_DocType_ID, 
@@ -45,10 +44,21 @@ SELECT
 	m.SalesRep_ID, 
 	m.User1_ID, 
 	m.User2_ID,
-	m.M_Movement_ID FTA_RV_M_Movement_ID
-
+	m.M_Movement_ID FTA_RV_M_Movement_ID,
+	COALESCE(bp.Value, bp.TaxID) AS BPTaxID, 
+	bp.Name::text || COALESCE(bp.Name2, ''::character varying)::text AS BPName, 
+	oi.TaxID, lo.C_Location_ID AS Org_Location_ID, 
+	m.C_BPartner_Location_ID
 FROM M_Movement m
 LEFT JOIN FTA_Driver d ON (m.FTA_Driver_ID = d.FTA_Driver_ID)
 LEFT JOIN FTA_Vehicle v ON (m.FTA_Vehicle_ID = v.FTA_Vehicle_ID)
 LEFT JOIN M_Shipper s ON (m.M_Shipper_ID = s.M_Shipper_ID)
+LEFT JOIN C_BPartner bp ON (bp.C_BPartner_ID = m.C_BPartner_ID)
+LEFT JOIN AD_OrgInfo oi ON (oi.AD_Org_ID = m.AD_Org_ID )
+LEFT JOIN C_Location lo ON (lo.C_Location_ID = oi.C_Location_ID)
+
+
+
+WHERE m.M_Movement_ID =1000099
 ;
+
