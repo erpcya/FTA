38596d34fa71d63d13b49e1f900a41e71df533a2
Yamel Senih <yamelsenih@gmail.com>
2013-12-23 10:57:50 -0530

    Add Load Order View Event...

---
+++ migration/postgresql/213UpdateTableFTA_InterestType.sql
@@ -0,0 +1,20 @@
+-- Oct 25, 2013 5:11:26 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Table_Trl SET Name='Tipo de Interés',Updated=TO_TIMESTAMP('2013-10-25 17:11:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=53647 AND AD_Language='es_MX'
+;
+
+-- Oct 25, 2013 5:12:33 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Element_Trl SET Name='Tipo de Interés',PrintName='Tipo de Interés',Updated=TO_TIMESTAMP('2013-10-25 17:12:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=56547 AND AD_Language='es_MX'
+;
+
+-- Oct 25, 2013 5:14:22 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Window_Trl SET Name='Tipo de Interés',Description='Tipo de Interés',Updated=TO_TIMESTAMP('2013-10-25 17:14:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=53293 AND AD_Language='es_MX'
+;
+
+-- Oct 25, 2013 5:14:28 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Tab_Trl SET Name='Tipo de Interés',Description='Tipo de Interés',Updated=TO_TIMESTAMP('2013-10-25 17:14:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53791 AND AD_Language='es_MX'
+;
+
