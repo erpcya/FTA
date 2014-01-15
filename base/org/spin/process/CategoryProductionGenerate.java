5a2060027158251ef11cdd6216dd60ed636d643b
Yamel Senih <Yamel Senih@localhost>
2014-01-15 17:59:32 +0100

    Add Changes in Category Production Generate

--- base/org/spin/process/CategoryProductionGenerate.java
+++ base/org/spin/process/CategoryProductionGenerate.java
@@ -16,10 +16,18 @@
  *****************************************************************************/
 package org.spin.process;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 
+import org.adempiere.exceptions.AdempiereException;
+import org.compiere.process.DocAction;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
+import org.compiere.util.DB;
+import org.spin.model.MFTACategoryCalc;
+import org.spin.model.MFTACategoryCalcGroup;
 
 /**
  * Generate Category Production
@@ -32,6 +40,8 @@ public class CategoryProductionGenerate extends SvrProcess {
 	private int			p_AD_Org_ID = 0;
 	/**	Warehouse					*/
 	private int			p_M_Warehouse_ID = 0;
+	/**	Operation Type				*/
+	private String 		p_OperationType = null;
 	/**	Document Date from			*/
 	private Timestamp 	p_DateDoc = null;
 	/**	Document Date to			*/
@@ -41,7 +51,7 @@ public class CategoryProductionGenerate extends SvrProcess {
 	/**	Category Calculation		*/
 	private int 		p_FTA_CategoryCalc_ID = 0;
 	/**	Document Action				*/
-	private String 		p_DocAction	= null;
+	private String 		p_DocAction	= DocAction.ACTION_Prepare;
 	
 	@Override
 	protected void prepare() {
@@ -52,12 +62,14 @@ public class CategoryProductionGenerate extends SvrProcess {
 				;
 			else if(name.equals("AD_Org_ID"))
 				p_AD_Org_ID = para.getParameterAsInt();
-			else if(name.equals("FTA_CreditDefinition_ID"))
+			else if(name.equals("M_Warehouse_ID"))
 				p_M_Warehouse_ID = para.getParameterAsInt();
+			else if(name.equals("OperationType"))
+				p_OperationType = (String) para.getParameter();
 			else if(name.equals("DateDoc")){
 				p_DateDoc = (Timestamp) para.getParameter();
 				p_DateDoc_To = (Timestamp) para.getParameter_To();
-			} else if(name.equals("FTA_CAtegoryCalcGroup_ID"))
+			} else if(name.equals("FTA_CategoryCalcGroup_ID"))
 				p_FTA_CategoryCalcGroup_ID = para.getParameterAsInt();
 			else if(name.equals("FTA_CategoryCalc_ID"))
 				p_FTA_CategoryCalc_ID = para.getParameterAsInt();
@@ -68,7 +80,71 @@ public class CategoryProductionGenerate extends SvrProcess {
 
 	@Override
 	protected String doIt() throws Exception {
+		//	Valid Operation Type
+		if(p_OperationType == null)
+			throw new AdempiereException("@OperationType@ @NotFound@");
+		//	Valid category calc group and category calc
+		if(p_FTA_CategoryCalcGroup_ID == 0
+				&& p_FTA_CategoryCalc_ID == 0)
+			throw new AdempiereException("@FTA_CategoryCalcGroup_ID@ @AND@ @FTA_CategoryCalc_ID@ @NotFound@");
+		//	Message
+		StringBuffer m_msg = new StringBuffer();
+		//	Get From Group
+		if(p_FTA_CategoryCalcGroup_ID != 0){
+			MFTACategoryCalcGroup m_ccGroup = new MFTACategoryCalcGroup(getCtx(), p_FTA_CategoryCalcGroup_ID, get_TrxName());
+			MFTACategoryCalc [] m_categoryCalc = m_ccGroup.getLines(false);
+			//	generate all from group
+			for(MFTACategoryCalc m_cCalc : m_categoryCalc){
+				String msg = productionGenerate(m_cCalc);
+				//	Get Message
+				if(msg != null){
+					if(m_msg.length() > 0)
+						m_msg.append("\n").append(msg);
+					else
+						m_msg.append(msg);
+				}
+			}
+		} else if(p_FTA_CategoryCalc_ID != 0){
+			MFTACategoryCalc m_cCalc = new MFTACategoryCalc(getCtx(), p_FTA_CategoryCalc_ID, get_TrxName());
+			String msg = productionGenerate(m_cCalc);
+			//	Get Message
+			if(msg != null)
+				m_msg.append(msg);
+		}
+		//	
+		return m_msg.toString();
+	}
+	
+	/**
+	 * Production Generate from Category Calculation
+	 * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a> 15/01/2014, 16:20:33
+	 * @param m_cCalc
+	 * @return
+	 * @return String
+	 * @throws SQLException 
+	 */
+	private String productionGenerate(MFTACategoryCalc m_cCalc) throws SQLException{
+		PreparedStatement ps=null;
+		ResultSet rs =null;
+		
+		StringBuffer sql = new StringBuffer("SELECT qa.M_Product_ID, qa.QualityAnalysis_ID, rw.DocumentNo, rw.NetWeight, rw.PayWeight " +
+				"FROM FTA_RecordWeight rw " +
+				"INNER JOIN FTA_QualityAnalysis qa ON(qa.FTA_QualityAnalysis_ID = rw.FTA_QualityAnalysis_ID) " +
+				"INNER JOIN FTA_CategoryCalc cc ON(cc.M_Product_ID = qa.M_Product_ID) " +
+				"INNER JOIN FTA_RV_AttributeValue av ON(av.M_AttributeSetInstance_ID = qa.QualityAnalysis_ID) ");
+		
+		//	Group By
+		sql.append("GROUP BY qa.M_Product_ID, rw.FTA_RecordWeight_ID, qa.QualityAnalysis_ID ");
+		//	Order By
+		sql.append("ORDER BY qa.M_Product_ID, rw.FTA_RecordWeight_ID");
+		ps = DB.prepareStatement(sql.toString(), get_TrxName());
+		ps.setInt(1, m_cCalc.getFTA_CategoryCalc_ID());
+		rs = ps.executeQuery();
+		
+		while(rs.next()){
+			
+		}
+		
 		return null;
 	}
-
 }
