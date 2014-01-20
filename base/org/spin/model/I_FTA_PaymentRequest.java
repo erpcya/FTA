f6ebdad61f1dc928d1379ee43cd253e01a309ff9
Dixon Martinez <Dixon Martinez@localhost>
2014-01-20 10:32:05 -0530

    Update Class Model Record Weight and Payment Request

--- base/org/spin/model/I_FTA_PaymentRequest.java
+++ base/org/spin/model/I_FTA_PaymentRequest.java
@@ -123,6 +123,21 @@ public interface I_FTA_PaymentRequest
 
 	public I_C_DocType getC_DocType() throws RuntimeException;
 
+    /** Column name C_Invoice_ID */
+    public static final String COLUMNNAME_C_Invoice_ID = "C_Invoice_ID";
+
+	/** Set Invoice.
+	  * Invoice Identifier
+	  */
+	public void setC_Invoice_ID (int C_Invoice_ID);
+
+	/** Get Invoice.
+	  * Invoice Identifier
+	  */
+	public int getC_Invoice_ID();
+
+	public I_C_Invoice getC_Invoice() throws RuntimeException;
+
     /** Column name C_PaySelectionCheck_ID */
     public static final String COLUMNNAME_C_PaySelectionCheck_ID = "C_PaySelectionCheck_ID";
 
