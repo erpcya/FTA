c3c74c4616f565b023fd82026227aef4567c78c6
carlosaparadam <carlosaparadam@localhost>
2014-01-24 20:01:08 -0530

    Add Callout Load Order in Record Weight

---
+++ migration/oracle/360AddCallOutFTALoadOrderInRecordWeight_oracle.sql
@@ -0,0 +1,5 @@
+-- Jan 24, 2014 7:38:31 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET Callout='org.spin.model.CalloutRecordWeight.product',Updated=TO_DATE('2014-01-24 19:38:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=70645
+;
+
