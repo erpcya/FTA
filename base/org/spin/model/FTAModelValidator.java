0626b61c2978ee85c2268e8b0e9fefde7151057a
Yamel Senih <yamelsenih@gmail.com>
2014-01-30 15:16:26 +0100

    Fixed error with copy Credit fact manual in invoice from order

--- base/org/spin/model/FTAModelValidator.java
+++ base/org/spin/model/FTAModelValidator.java
@@ -93,9 +93,11 @@ public class FTAModelValidator implements ModelValidator {
 				MOrder order = new MOrder(Env.getCtx(), m_C_Order_ID, invoice.get_TableName());
 				int m_FTA_FarmerCredit_ID = order.get_ValueAsInt("FTA_FarmerCredit_ID");
 				boolean m_IsExceedCreditLimit = order.get_ValueAsBoolean("IsExceedCreditLimit");
+				boolean m_IsCreditFactManual = order.get_ValueAsBoolean("IsCreditFactManual");
 				if(m_FTA_FarmerCredit_ID != 0) {
 					invoice.set_ValueOfColumn("FTA_FarmerCredit_ID", m_FTA_FarmerCredit_ID);
 					invoice.set_ValueOfColumn("IsExceedCreditLimit", m_IsExceedCreditLimit);
+					invoice.set_ValueOfColumn("IsCreditFactManual", m_IsCreditFactManual);
 				}
 				//	
 				log.info(po.toString());
