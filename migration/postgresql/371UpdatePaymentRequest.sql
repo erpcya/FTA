689558e9310052c02fb2a92af685caefbf581d51
Dixon Martinez <dixon.22martinez@gmail.com>
2014-02-06 16:48:48 -0530

    Update Scripts

--- migration/postgresql/371UpdatePaymentRequest.sql
+++ migration/postgresql/371UpdatePaymentRequest.sql
@@ -8,3 +8,23 @@ UPDATE AD_Column SET IsUpdateable='N',Updated=TO_TIMESTAMP('2014-02-06 16:55:36'
 UPDATE AD_Column SET IsUpdateable='N',Updated=TO_TIMESTAMP('2014-02-06 16:55:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=69381
 ;
 
+-- Feb 6, 2014 5:42:57 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET DisplayLogic='@M_ProductionLine_ID@ !0',Updated=TO_TIMESTAMP('2014-02-06 17:42:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=72148
+;
+
+-- Feb 6, 2014 5:43:20 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET IsUpdateable='N',Updated=TO_TIMESTAMP('2014-02-06 17:43:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=71486
+;
+
+-- Feb 6, 2014 5:43:28 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET DisplayLength=40,Updated=TO_TIMESTAMP('2014-02-06 17:43:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=72264
+;
+
+-- Feb 6, 2014 5:43:31 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Field SET DisplayLength=10,Updated=TO_TIMESTAMP('2014-02-06 17:43:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=72264
+;
+
