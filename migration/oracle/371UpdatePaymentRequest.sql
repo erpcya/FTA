26d131fab28a8e5fca08e24d618a06b4dc57d321
Dixon Martinez <dixon.22martinez@gmail.com>
2014-02-06 16:41:20 -0530

    Update Payment Request

---
+++ migration/oracle/371UpdatePaymentRequest.sql
@@ -0,0 +1,10 @@
+-- Feb 6, 2014 4:55:36 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET IsUpdateable='N',Updated=TO_DATE('2014-02-06 16:55:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=71337
+;
+
+-- Feb 6, 2014 4:55:42 PM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET IsUpdateable='N',Updated=TO_DATE('2014-02-06 16:55:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=69381
+;
+
