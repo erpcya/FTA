70065d4c2f5bdadc34ac0f30fedd9ef6c8bf2e63
Dixon Martinez <dixon.22martinez@gmail.com>
2014-02-11 21:05:38 -0530

    Update Record Weight

--- migration/postgresql/861AddViewFTA_RV_RecordWeight.sql
+++ migration/postgresql/861AddViewFTA_RV_RecordWeight.sql
@@ -9,7 +9,8 @@ SELECT
 	rw.FTA_RecordWeight_ID, rw.GrossWeight, rw.InDate, rw.IsSOTrx, rw.NetWeight, rw.OutDate, 
 	rw.TareWeight, rw.WeightStatus, 
 	dr.FTA_Driver_ID, dr.Value, vh.FTA_Vehicle_ID, vh.VehiclePlate, 
-	COALESCE(cp.C_BPartner_ID,o.C_BPartner_ID)C_BPartner_ID, et.FTA_MobilizationGuide_ID, COALESCE(qaa.M_Product_ID,qa.M_Product_ID,lo.M_Product_ID,ol.M_Product_ID) M_Product_ID, qa.QualityAnalysis_ID,
+	COALESCE(cp.C_BPartner_ID,o.C_BPartner_ID)C_BPartner_ID, et.FTA_MobilizationGuide_ID, COALESCE(qaa.M_Product_ID,qa.M_Product_ID,lo.M_Product_ID,ol.M_Product_ID) M_Product_ID, 
+	COALESCE(qaa.QualityAnalysis_ID, qa.QualityAnalysis_ID)QualityAnalysis_ID,
 	oi.C_Location_ID AS Org_Location_ID, oi.TaxID,mg.M_Warehouse_ID, rw.FTA_RecordWeight_ID AS FTA_RV_RecordWeight_ID,
 	rw.InDate InTime,
 	rw.OutDate OutTime,
@@ -49,15 +50,16 @@ LEFT JOIN
 	(
 		SELECT 
 			qa.FTA_QualityAnalysis_ID,
-			qa.M_Product_ID
+			qa.M_Product_ID,
+			QualityAnalysis_ID
 		FROM FTA_QualityAnalysis qa 
 		
 	) qaa ON (qaa.FTA_QualityAnalysis_ID = rw.FTA_QualityAnalysis_ID)
 WHERE rw.DocStatus IN ('CO','CL')
---AND rw.FTA_RecordWeight_ID IN (1000078,1000101)
+--AND rw.FTA_RecordWeight_ID IN (1000116)
 	
 --AND rw.OperationType = 'RMR'
 
-
+--SELECT * FROM FTA_QualityAnalysis WHERE QualityAnalysis_ID = 1039677
 
 --SELECT COUNT(*) FROM FTA_RecordWeight WHERE OperationType = 'RMR'
