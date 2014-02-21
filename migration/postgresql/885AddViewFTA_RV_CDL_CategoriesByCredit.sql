a00e2eabb261f57e84f03e9c7ed6a45ad0fb3fea
Carlos Parada <carlosaparadam@gmail.com>
2014-02-20 21:03:28 -0530

    Add Line Description To Browse Generate Credit Debit

--- migration/postgresql/885AddViewFTA_RV_CDL_CategoriesByCredit.sql
+++ migration/postgresql/885AddViewFTA_RV_CDL_CategoriesByCredit.sql
@@ -1,5 +1,6 @@
 ﻿-- 
 --DROP VIEW FTA_RV_CDL_CategoriesByCredit;
+-- 
 CREATE OR REPLACE VIEW FTA_RV_CDL_CategoriesByCredit AS 
 SELECT  fc.AD_Client_ID,
 	fc.AD_Org_ID,
@@ -12,7 +13,15 @@ SELECT  fc.AD_Client_ID,
 	cdl.IsExceedCreditLimit,
 	fc.ApprovedQty,
 	fc.ApprovedQty * cdl.Amt  ApprovedAmt,
-	fact.Amt AllocatedAmt
+	fact.Amt AllocatedAmt,
+	Case 
+	  When cdl.M_Product_Category_ID Is Not Null 
+	    Then mpc.Name 
+	  When cdl.M_Product_ID Is Not Null 
+	    Then mp.Name 
+	  When cdl.C_Charge_ID Is Not Null 
+	    Then cch.Name
+	End || COALESCE(cdl.Description, '') LineDescription--Description Invoice
 FROM
 FTA_FarmerCredit fc
 INNER JOIN FTA_CreditDefinition cd ON fc.FTA_CreditDefinition_ID= cd.FTA_CreditDefinition_ID
@@ -24,4 +33,7 @@ LEFT JOIN (SELECT
 	   WHERE AD_Table_ID = 318
 	   GROUP BY FTA_CreditDefinitionLine_ID
 		) fact On fact.FTA_CreditDefinitionLine_ID=cdl.FTA_CreditDefinitionLine_ID
+LEFT JOIN M_Product_Category mpc ON mpc.M_Product_Category_ID=cdl.M_Product_Category_ID
+LEFT JOIN M_Product mp ON mp.M_Product_ID=cdl.M_Product_ID
+LEFT JOIN C_Charge cch ON cch.C_Charge_ID=cdl.C_Charge_ID
 ;
