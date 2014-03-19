78d27c7df9346151b99392a504ccd22147a5daea
Yamel Senih <yamelsenih@gmail.com>
2014-03-19 20:07:57 +0100

    Add Farmer Receipt Report

---
+++ migration/postgresql/321AddCalloutOrderIntoTableFTA_EntryTIcket.sql
@@ -0,0 +1,25 @@
+-- Dec 12, 2013 11:19:37 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET Callout='org.spin.model.CalloutLoadOrder.basemobilizationGuide',Updated=TO_TIMESTAMP('2013-12-12 11:19:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66883
+;
+
+-- Dec 12, 2013 11:20:42 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET Callout='org.spin.model.CallOutEntryTicket.mobilizationGuide',Updated=TO_TIMESTAMP('2013-12-12 11:20:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66883
+;
+
+-- Dec 12, 2013 11:21:29 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET Callout=NULL,Updated=TO_TIMESTAMP('2013-12-12 11:21:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66883
+;
+
+-- Dec 12, 2013 11:21:38 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET Callout='org.spin.model.CallOutEntryTicket.mobilizationGuide',Updated=TO_TIMESTAMP('2013-12-12 11:21:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=70630
+;
+
+-- Dec 12, 2013 11:30:22 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Column SET Callout='org.spin.model.CallOutEntryTicket.order',Updated=TO_TIMESTAMP('2013-12-12 11:30:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=70630
+;
+
