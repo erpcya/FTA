8c2ea0248e2273909e9669b016998837dc7f5e46
Dixon Martinez <dixon.22martinez@gmail.com>
2014-03-27 11:04:53 -0530

    Update Class Model

--- base/org/spin/model/X_FTA_ProductListApproved.java
+++ base/org/spin/model/X_FTA_ProductListApproved.java
@@ -32,7 +32,7 @@ public class X_FTA_ProductListApproved extends PO implements I_FTA_ProductListAp
 	/**
 	 *
 	 */
-	private static final long serialVersionUID = 20140326L;
+	private static final long serialVersionUID = 20140327L;
 
     /** Standard Constructor */
     public X_FTA_ProductListApproved (Properties ctx, int FTA_ProductListApproved_ID, String trxName)
