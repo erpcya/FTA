0e69298d507c0c2b7e60d5b104d2fde742e63c51
Dixon Martinez <Dixon Martinez@localhost>
2014-04-29 09:33:24 -0530

    Update callout Technical Form

--- base/org/spin/model/CalloutTechnicalForm.java
+++ base/org/spin/model/CalloutTechnicalForm.java
@@ -22,6 +22,10 @@ import java.util.Properties;
 import org.compiere.model.CalloutEngine;
 import org.compiere.model.GridField;
 import org.compiere.model.GridTab;
+import org.compiere.model.MProduct;
+import org.compiere.model.MSysConfig;
+import org.compiere.util.DB;
+import org.compiere.util.Env;
 
 /**
  * @author <a href="mailto:yamelsenih@gmail.com">Yamel Senih</a>
@@ -67,6 +71,65 @@ public class CalloutTechnicalForm extends CalloutEngine {
 			return "";
 		//	Set Quantity
 		mTab.setValue("C_UOM_ID", m_Dosage_Uom_ID);
+		return "";
+	}
+	/**
+	 * 
+	 * @author <a href="mailto:dixon.22martinez@gmail.com">Dixon Martinez</a> 29/04/2014, 09:56:05
+	 * @param ctx
+	 * @param WindowNo
+	 * @param mTab
+	 * @param mField
+	 * @param value
+	 * @return
+	 * @return String
+	 */
+	public String product (Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value){
+		if(MSysConfig.getBooleanValue("VALID_QTY_ON_HAND_IN_TECHNICAL_FORM", false)){
+
+			Integer M_Product_ID = (Integer) mTab.getValue("M_Product_ID");
+			Integer p_M_Warehouse_ID = (Integer) mTab.getValue("M_Warehouse_ID");
+			BigDecimal qty = (BigDecimal) mTab.getValue("Qty");
+			
+			if(qty == null
+					|| qty.equals(Env.ZERO))
+				return "";
+			
+			if (M_Product_ID == null 
+					|| M_Product_ID.intValue() == 0)
+				return "";
+				
+			MProduct product = 
+					MProduct.get (ctx, M_Product_ID.intValue());
+			
+			if(product.isStocked()){
+				
+				StringBuffer sql = new StringBuffer(" SELECT SUM(QtyOnHand - QtyReserved) QtyOnHand "
+						+ " FROM M_Storage "
+						+ " WHERE M_Product_ID = ?");
+				
+				//	Warehouse
+				if(p_M_Warehouse_ID != null 
+						&& p_M_Warehouse_ID.intValue() != 0)
+					sql.append("AND EXISTS(SELECT 1 " +
+							"FROM M_Locator l " +
+							"WHERE l.M_Locator_ID = M_Storage.M_Locator_ID " +
+							"AND l.M_Warehouse_ID = ").append(p_M_Warehouse_ID).append(") ");
+				
+						
+				BigDecimal qtyOnHand = DB.getSQLValueBD(null, sql.toString(), product.get_ID());
+				
+				if(qtyOnHand == null)
+					qtyOnHand = Env.ZERO;
+				
+				if(qtyOnHand.compareTo(qty) < 0){
+					mTab.fireDataStatusEEvent ("InsufficientQtyAvailable", qtyOnHand.toString(), false);
+					return "";
+				}
+			}
+
+		}
+		
 		return "";
 	}
 }
