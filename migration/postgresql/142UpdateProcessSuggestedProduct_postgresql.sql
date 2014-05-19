d111bf85107e3a696c6ce7b20f948f0ddf1f10be
Dixon Martinez <dixon.22martinez@gmail.com>
2014-05-19 01:13:57 -0530

    Add support for Parent Farmer Credit

---
+++ migration/postgresql/142UpdateProcessSuggestedProduct_postgresql.sql
@@ -0,0 +1,5 @@
+-- Oct 11, 2013 5:54:25 PM VET
+-- SFAndroid Contribution
+UPDATE AD_Process SET Classname='org.spin.process.SuggestedProducts',Updated=TO_TIMESTAMP('2013-10-11 17:54:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=53480
+;
+
