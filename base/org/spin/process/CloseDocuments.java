537f75085be2e51d0ab6f5fceb42ae723390e556
Carlos Parada <carlosaparadam@gmail.com>
2014-05-19 21:06:42 -0530

    Correct Process Close Documents

--- base/org/spin/process/CloseDocuments.java
+++ base/org/spin/process/CloseDocuments.java
@@ -2,8 +2,8 @@ package org.spin.process;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.util.List;
-
 import org.compiere.model.MBPartner;
 import org.compiere.model.MInvoice;
 import org.compiere.model.MInvoiceLine;
@@ -11,6 +11,11 @@ import org.compiere.model.Query;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.DB;
+import org.compiere.util.Env;
+import org.spin.model.MFTAAllocation;
+import org.spin.model.MFTAAllocationLine;
+import org.spin.model.MFTACreditDefinition;
+import org.spin.model.MFTAFarmerCredit;
 import org.spin.model.MFTAFarmerLiquidation;
 
 public class CloseDocuments extends SvrProcess{
@@ -24,11 +29,32 @@ public class CloseDocuments extends SvrProcess{
 	/**Parameter Charge*/
 	private int p_C_Charge_ID = 0;
 	
-	/**Invoice Generated */
-	private MInvoice m_InvoiceGen = null;
+	/** Dated Invoiced*/
+	private Timestamp p_DateInvoiced = null;
 	
-	/** Invoice Line Generated */
-	private MInvoiceLine m_InvoiceLineGen = null;
+	/**Invoice Generated Credit*/
+	private MInvoice m_InvoiceGenCR = null;
+	
+	/** Invoice Line Generated Credit*/
+	private MInvoiceLine m_InvoiceLineGenCR = null;
+	
+	/**Invoice Generated Debit*/
+	private MInvoice m_InvoiceGenDB = null;
+	
+	/** Invoice Line Generated Debit*/
+	private MInvoiceLine m_InvoiceLineGenDB = null;
+	
+	/** Allocation */
+	private MFTAAllocation m_FTAAlloc = null;
+	
+	/** Farmer Credit*/ 
+	private MFTAFarmerCredit m_FarmerCredit = null;
+	
+	/** Farmer Credit Defenition*/ 
+	private MFTACreditDefinition m_CreditDefinition = null;
+	
+	/** Documents Generated*/
+	private int m_Generated = 0;
 	
 	/** Sql*/
 	private String m_Sql = "";
@@ -37,18 +63,20 @@ public class CloseDocuments extends SvrProcess{
 	protected void prepare() {
 		// TODO Auto-generated method stub
 		for (ProcessInfoParameter para:getParameter()){
-			
 			String name = para.getParameterName();
 			if (para.getParameter() == null)
 				;
 			else if(name.equals("FTA_FarmerCredit_ID"))
 				p_FTA_FarmerCredit_ID = para.getParameterAsInt();
-			
-			else if(name.equals("C_DocType_ID"))
+
+			else if(name.equals("C_DocTypeTarget_ID"))
 				p_C_DocType_ID = para.getParameterAsInt();
-			
+
 			else if(name.equals("C_Charge_ID"))
 				p_C_Charge_ID = para.getParameterAsInt();
+
+			else if(name.equals("DateInvoiced"))
+				p_DateInvoiced = (Timestamp)para.getParameter();
 		}
 	}
 
@@ -63,18 +91,22 @@ public class CloseDocuments extends SvrProcess{
 				" AND LiquidationAvailable(FTA_FarmerLiquidation.FTA_FarmerLiquidation_ID) <>0 " +
 				" AND FTA_FarmerLiquidation.FTA_FarmerCredit_ID = ? "
 				, get_TrxName()).setParameters(MFTAFarmerLiquidation.Table_ID,p_FTA_FarmerCredit_ID).list();
-		
+
 		for (MFTAFarmerLiquidation liq:liqs)
 			addLog("@Error@ @FTA_FarmerLiquidation_ID@  " + liq.getDocumentNo() + " @OpenAmt@");
-		
-		
+
 		if (liqs.size()>0)
 			return "@Error@";
-		
 		//
-		
 		//Match Invoiced Pending 
+
+		m_FarmerCredit = new MFTAFarmerCredit(getCtx(), p_FTA_FarmerCredit_ID, get_TrxName());
+		m_CreditDefinition = new MFTACreditDefinition(getCtx(), m_FarmerCredit.getFTA_CreditDefinition_ID(), get_TrxName()); 
 		
+		if (m_CreditDefinition.get_ValueAsInt("C_DocTypeClosedAR_ID")==0 || m_CreditDefinition.get_ValueAsInt("C_DocTypeClosedAP_ID")==0){
+			rollback();
+			return "@Error@ @Invalid@ @C_DocTypeClosedAR_ID@ @OR@ @C_DocTypeClosedAP_ID@" ;
+		}
 		
 		m_Sql = "SELECT C_Invoice_ID, invoiceOpenFTA(fact.Record_ID,null) OpenAmt " +
 				"FROM " + 
@@ -84,7 +116,7 @@ public class CloseDocuments extends SvrProcess{
 				"FROM FTA_Fact " + 
 				"WHERE AD_Table_ID = ? " +
 				") fact ON ci.C_invoice_ID = fact.Record_ID " + 
-				"WHERE invoiceOpenFTA(fact.Record_ID,null) <>0 " +
+				"WHERE invoiceOpenFTA(fact.Record_ID,null) <>0 AND " +
 				"ci.FTA_FarmerCredit_ID = ? ";
 		
 		ps = DB.prepareStatement(m_Sql, get_TrxName());
@@ -94,27 +126,101 @@ public class CloseDocuments extends SvrProcess{
 		rs = ps.executeQuery();
 		
 		while (rs.next()){
-		
+			// Generate Invoice Credit Memo
 			MInvoice inv = new MInvoice(getCtx(), rs.getInt("C_Invoice_ID"), get_TrxName());
-			if (m_InvoiceGen == null)
-				m_InvoiceGen = new MInvoice(getCtx(), 0, get_TrxName());
-			
-			m_InvoiceGen.setBPartner(new MBPartner(getCtx(), inv.getC_BPartner_ID(), get_TrxName()));
-			m_InvoiceGen.setC_DocTypeTarget_ID(p_C_DocType_ID);
-			 
-			if (m_InvoiceLineGen == null){
-				m_InvoiceLineGen = new MInvoiceLine(m_InvoiceGen);
-				m_InvoiceLineGen.setC_Charge_ID(p_C_Charge_ID);
+			if (m_InvoiceGenCR == null){
+				m_InvoiceGenCR = new MInvoice(getCtx(), 0, get_TrxName());
+				m_InvoiceGenCR.setBPartner(new MBPartner(getCtx(), inv.getC_BPartner_ID(), get_TrxName()));
+				m_InvoiceGenCR.setC_DocTypeTarget_ID(p_C_DocType_ID);
+				m_InvoiceGenCR.setDateInvoiced(p_DateInvoiced);
+				m_InvoiceGenCR.setDateAcct(p_DateInvoiced);
+				m_InvoiceGenCR.set_ValueOfColumn("FTA_FarmerCredit_ID", p_FTA_FarmerCredit_ID);
+				m_InvoiceGenCR.save(get_TrxName());
 			}
-			m_InvoiceLineGen.setPriceActual(m_InvoiceLineGen.getPriceActual().add(rs.getBigDecimal("OpenAmt")));
+			
+			if (m_InvoiceLineGenCR == null){
+				m_InvoiceLineGenCR = new MInvoiceLine(m_InvoiceGenCR);
+				m_InvoiceLineGenCR.setC_Charge_ID(p_C_Charge_ID);
+				m_InvoiceLineGenCR.setQty(Env.ONE);
+			}
+			
+			m_InvoiceLineGenCR.setPrice(m_InvoiceLineGenCR.getPriceEntered().add(rs.getBigDecimal("OpenAmt")));
+			m_InvoiceLineGenCR.save(get_TrxName());
+			
+			//Allocate Invoice Credit Memo With Pending Documents
+			if (m_FTAAlloc == null){
+				m_FTAAlloc = new MFTAAllocation(getCtx(), 0, get_TrxName());
+				m_FTAAlloc.setDateDoc(p_DateInvoiced);
+				m_FTAAlloc.setC_Currency_ID(Env.getContextAsInt(getCtx(), "$C_Currency_ID"));
+				m_FTAAlloc.setFTA_FarmerCredit_ID(p_FTA_FarmerCredit_ID);
+				m_FTAAlloc.save(get_TrxName());
+			}
+			
+			MFTAAllocationLine m_FTAAllocLine = new MFTAAllocationLine(m_FTAAlloc);
+			m_FTAAllocLine.setC_Invoice_ID(inv.getC_Invoice_ID());
+			m_FTAAllocLine.setC_BPartner_ID(inv.getC_BPartner_ID());
+			m_FTAAllocLine.setAmount(rs.getBigDecimal("OpenAmt"));
+			m_FTAAllocLine.save(get_TrxName());
 		}
-		
-		
-		
-		
+
+		//Process Invoiced Credit Memo
+		if (!m_InvoiceGenCR.processIt(MInvoice.DOCACTION_Complete)){
+			rollback();
+			return "@Error@ @Processing@ @C_Invoice_ID@";
+		}
+		addLog("@Generated@ @C_Invoice_ID@ : " + m_InvoiceGenCR.getDocumentNo());
+		m_Generated ++;
+
+		//Alloc Credit Document
+		MFTAAllocationLine m_FTAAllocLine = new MFTAAllocationLine(m_FTAAlloc);
+		m_FTAAllocLine.setC_Invoice_ID(m_InvoiceGenCR.getC_Invoice_ID());
+		m_FTAAllocLine.setC_BPartner_ID(m_InvoiceGenCR.getC_BPartner_ID());
+		m_FTAAllocLine.setAmount(m_InvoiceGenCR.getGrandTotal().negate());
+		m_FTAAllocLine.save(get_TrxName());
+
+		//Process Allocation
+		if (!m_FTAAlloc.processIt(MFTAAllocation.DOCACTION_Complete)){
+			rollback();
+			return "@Error@ @Processing@ @FTA_Allocation_ID@";
+		}
+		addLog("@Generated@ @FTA_Allocation_ID@ : " + m_FTAAlloc.getDocumentNo());
+		m_Generated ++;
+
+		//Generate Debit Document for Closed 
+		m_InvoiceGenDB = new MInvoice(getCtx(), 0, get_TrxName());
+		m_InvoiceGenDB.setBPartner(new MBPartner(getCtx(), m_FarmerCredit.getC_BPartner_ID(), get_TrxName()));
+		m_InvoiceGenDB.setC_DocTypeTarget_ID(
+				(m_InvoiceLineGenCR.getPriceEntered().signum()!=-1?
+						m_CreditDefinition.get_ValueAsInt("C_DocTypeClosedAR_ID")
+							:m_CreditDefinition.get_ValueAsInt("C_DocTypeClosedAP_ID")));
+		m_InvoiceGenDB.setIsSOTrx((m_InvoiceLineGenCR.getPriceEntered().signum()!=-1?
+				true
+				:false));
+		m_InvoiceGenDB.setDateInvoiced(p_DateInvoiced);
+		m_InvoiceGenDB.setDateAcct(p_DateInvoiced);
+		m_InvoiceGenDB.set_ValueOfColumn("FTA_FarmerCredit_ID", p_FTA_FarmerCredit_ID);
+		m_InvoiceGenDB.save(get_TrxName());
+
+		m_InvoiceLineGenDB = new MInvoiceLine(m_InvoiceGenDB);
+		m_InvoiceLineGenDB.setC_Charge_ID(p_C_Charge_ID);
+		m_InvoiceLineGenDB.setQty(Env.ONE);
+		m_InvoiceLineGenDB.setPrice(
+				(m_InvoiceLineGenCR.getPriceEntered().signum()!=-1?
+						m_InvoiceLineGenCR.getPriceEntered()
+							:m_InvoiceLineGenCR.getPriceEntered().negate())
+				);
+		m_InvoiceLineGenDB.save(get_TrxName());
+
+		//Process Debit Document 
+		if (!m_InvoiceGenDB.processIt(MInvoice.DOCACTION_Complete))	{
+			rollback();
+			return "@Error@ @Processing@ @C_Invoice_ID@";
+		}
+		addLog("@Generated@ @C_Invoice_ID@ : " + m_InvoiceGenDB.getDocumentNo());
+		m_Generated ++;
 		//
 		
-		return null;
+		return "@Generated@ " + m_Generated;
 	}
 
 }
---
+++ migration/postgresql/468AddProcessCloseDocuments_postgresql.sql
@@ -0,0 +1,90 @@
+-- 19/05/2014 10:56:39 AM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Process (AccessLevel,AD_Client_ID,AD_Org_ID,AD_Process_ID,Classname,CopyFromProcess,Created,CreatedBy,EntityType,IsActive,IsBetaFunctionality,IsDirectPrint,IsReport,IsServerProcess,Name,ShowHelp,Statistic_Count,Statistic_Seconds,Updated,UpdatedBy,Value) VALUES ('3',0,0,53631,'org.spin.process.CloseDocuments','N',TO_TIMESTAMP('2014-05-19 10:56:37','YYYY-MM-DD HH24:MI:SS'),100,'ECA02','Y','N','N','N','N','Close Documents From Farmer Credit','Y',0,0,TO_TIMESTAMP('2014-05-19 10:56:37','YYYY-MM-DD HH24:MI:SS'),100,'FTA_CloseDocuments Process')
+;
+
+-- 19/05/2014 10:56:39 AM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Process_Trl (AD_Language,AD_Process_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Process_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_ID=53631 AND NOT EXISTS (SELECT * FROM AD_Process_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_ID=t.AD_Process_ID)
+;
+
+-- 19/05/2014 10:57:00 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Process_Trl SET Name='Cerrar Documentos de Credito a Productor',Description='Cerrar Documentos de Credito a Productor',Updated=TO_TIMESTAMP('2014-05-19 10:56:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=53631 AND AD_Language='es_MX'
+;
+
+-- 19/05/2014 10:57:04 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Process_Trl SET Name='Cerrar Documentos de Credito a Productor',Description='Cerrar Documentos de Credito a Productor',Updated=TO_TIMESTAMP('2014-05-19 10:57:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=53631 AND AD_Language='es_VE'
+;
+
+-- 19/05/2014 10:57:49 AM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,AD_Val_Rule_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsCentrallyMaintained,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,187,0,53631,54777,30,52222,'C_BPartner_ID',TO_TIMESTAMP('2014-05-19 10:57:42','YYYY-MM-DD HH24:MI:SS'),100,'Identifies a Business Partner','ECA02',22,'A Business Partner is anyone with whom you transact.  This can include Vendor, Customer, Employee or Salesperson','Y','Y','Y','N','Business Partner ',10,TO_TIMESTAMP('2014-05-19 10:57:42','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 19/05/2014 10:57:49 AM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_Para_ID=54777 AND NOT EXISTS (SELECT * FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 19/05/2014 10:59:49 AM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Val_Rule (AD_Client_ID,AD_Org_ID,AD_Val_Rule_ID,Code,Created,CreatedBy,EntityType,IsActive,Name,Type,Updated,UpdatedBy) VALUES (0,0,52359,'FTA_FarmerCredit.C_BPartner_ID = @C_BPartner_ID@ AND FTA_FarmerCredit.DocStatus = ''CO'' ',TO_TIMESTAMP('2014-05-19 10:59:48','YYYY-MM-DD HH24:MI:SS'),100,'ECA02','Y','Farmer Credit From Farmer And Complete','S',TO_TIMESTAMP('2014-05-19 10:59:48','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 19/05/2014 10:59:58 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Val_Rule SET Name='Farmer Credit From Farmer And Completed',Updated=TO_TIMESTAMP('2014-05-19 10:59:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=52359
+;
+
+-- 19/05/2014 11:00:27 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Val_Rule SET Name='FTA_FarmerCredit From Farmer And Completed',Updated=TO_TIMESTAMP('2014-05-19 11:00:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=52359
+;
+
+-- 19/05/2014 11:00:38 AM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,AD_Val_Rule_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,IsActive,IsCentrallyMaintained,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,56084,0,53631,54778,19,52359,'FTA_FarmerCredit_ID',TO_TIMESTAMP('2014-05-19 11:00:36','YYYY-MM-DD HH24:MI:SS'),100,'Farmer Credit or Loan','ECA02',0,'Y','Y','N','N','Credit/Loan',20,TO_TIMESTAMP('2014-05-19 11:00:36','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 19/05/2014 11:00:38 AM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_Para_ID=54778 AND NOT EXISTS (SELECT * FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 19/05/2014 11:02:08 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Process_Para SET IsMandatory='Y',Updated=TO_TIMESTAMP('2014-05-19 11:02:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=54778
+;
+
+-- 19/05/2014 11:03:14 AM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,AD_Val_Rule_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsCentrallyMaintained,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,196,0,53631,54779,19,52226,'C_DocType_ID',TO_TIMESTAMP('2014-05-19 11:03:11','YYYY-MM-DD HH24:MI:SS'),100,'Document type or rules','ECA02',22,'The Document Type determines document sequence and processing rules','Y','Y','Y','N','Document Type',30,TO_TIMESTAMP('2014-05-19 11:03:11','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 19/05/2014 11:03:14 AM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_Para_ID=54779 AND NOT EXISTS (SELECT * FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 19/05/2014 11:04:02 AM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,AD_Reference_Value_ID,AD_Val_Rule_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsCentrallyMaintained,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,968,0,53631,54780,18,200,52030,'C_Charge_ID',TO_TIMESTAMP('2014-05-19 11:03:59','YYYY-MM-DD HH24:MI:SS'),100,'Additional document charges','ECA02',22,'The Charge indicates a type of Charge (Handling, Shipping, Restocking)','Y','Y','N','N','Charge',40,TO_TIMESTAMP('2014-05-19 11:03:59','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 19/05/2014 11:04:02 AM VET
+-- Farming Technical Assistance
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_Para_ID=54780 AND NOT EXISTS (SELECT * FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 19/05/2014 11:04:22 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Process_Para SET AD_Element_ID=197, AD_Reference_ID=18, ColumnName='C_DocTypeTarget_ID',Updated=TO_TIMESTAMP('2014-05-19 11:04:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=54779
+;
+
+-- 19/05/2014 11:04:25 AM VET
+-- Farming Technical Assistance
+UPDATE AD_Process_Para SET IsMandatory='Y',Updated=TO_TIMESTAMP('2014-05-19 11:04:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=54780
+;
+
