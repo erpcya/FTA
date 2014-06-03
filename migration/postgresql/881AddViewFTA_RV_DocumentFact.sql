5b23189aa2bd2681dbb1432e155e069586874c84
Dixon Martinez <Dixon Martinez@localhost>
2014-06-03 16:31:51 -0530

    Update Process Change qty approved to qty effective

--- migration/postgresql/881AddViewFTA_RV_DocumentFact.sql
+++ migration/postgresql/881AddViewFTA_RV_DocumentFact.sql
@@ -4,7 +4,11 @@ SELECT i.AD_Client_ID, i.AD_Org_ID, i.Updated, i.UpdatedBy, i.Created, i.Created
 i.C_BPartner_ID, i.DateInvoiced DateDoc, i.DocumentNo, i.Description::VARCHAR, 
 cd.FTA_CreditDefinition_ID, cdl.FTA_CreditDefinitionLine_ID, i.FTA_FarmerCredit_ID,  
 il.C_InvoiceLine_ID Line_ID, 
-il.LineNetAmt + (il.LineNetAmt * t.Rate / 100) Amt, (cdl.Amt * fc.ApprovedQty) SO_CreditLimit, 
+il.LineNetAmt + (il.LineNetAmt * t.Rate / 100) Amt, 
+--(cdl.Amt * fc.ApprovedQty) SO_CreditLimit, 
+(cdl.Amt * CASE WHEN fc.BasedOnEffectiveQuantity = 'Y' THEN f.EffectiveArea ELSE fc.ApprovedQty END
+
+) SO_CreditLimit, 
 COALESCE(SUM(ft.Amt), 0) SO_CreditUsed, 
 CASE 
 	WHEN i.IsExceedCreditLimit = 'Y' 
@@ -22,6 +26,7 @@ LEFT JOIN FTA_Fact ft ON(ft.FTA_FarmerCredit_ID = i.FTA_FarmerCredit_ID
 				AND ft.FTA_CreditDefinitionLine_ID = cdl.FTA_CreditDefinitionLine_ID AND ft.AD_Table_ID = 318)
 LEFT JOIN M_Product pr ON(pr.M_Product_ID = il.M_Product_ID) 
 LEFT JOIN C_Charge cr ON(cr.C_Charge_ID = il.C_Charge_ID)  
+LEFT JOIN FTA_Farming f ON  (f.FTA_FarmerCredit_ID = fc.FTA_FarmerCredit_ID)
 WHERE (
 		(cdl.M_Product_ID = il.M_Product_ID 
 			AND il.M_Product_ID IS NOT NULL) 
@@ -35,13 +40,18 @@ WHERE (
 GROUP BY i.AD_Client_ID, i.AD_Org_ID, i.Updated, i.UpdatedBy, i.Created, i.CreatedBy, i.IsActive, 
 i.C_Invoice_ID, i.C_BPartner_ID, i.DateInvoiced, i.DocumentNo, i.Description::VARCHAR, 
 cd.FTA_CreditDefinition_ID, cdl.FTA_CreditDefinitionLine_ID, i.FTA_FarmerCredit_ID,  
-i.C_Invoice_ID, il.C_InvoiceLine_ID, il.LineNetAmt, t.Rate, cdl.Amt, fc.ApprovedQty, cdl.Line, ft.AD_Table_ID
+i.C_Invoice_ID, il.C_InvoiceLine_ID, il.LineNetAmt, t.Rate, cdl.Amt, fc.ApprovedQty, cdl.Line, ft.AD_Table_ID,
+fc.BasedOnEffectiveQuantity,f.EffectiveArea
 UNION ALL
 SELECT o.AD_Client_ID, o.AD_Org_ID, o.Updated, o.UpdatedBy, o.Created, o.CreatedBy, o.IsActive, 
 o.C_BPartner_ID, o.DateOrdered DateDoc, o.DocumentNo, o.Description, 
 cd.FTA_CreditDefinition_ID, cdl.FTA_CreditDefinitionLine_ID, o.FTA_FarmerCredit_ID,  
 ol.C_OrderLine_ID Line_ID, 
-ol.LineNetAmt + (ol.LineNetAmt * t.Rate / 100) Amt, (cdl.Amt * fc.ApprovedQty) SO_CreditLimit, 
+ol.LineNetAmt + (ol.LineNetAmt * t.Rate / 100) Amt, 
+--(cdl.Amt * fc.ApprovedQty) SO_CreditLimit, 
+(cdl.Amt * CASE WHEN fc.BasedOnEffectiveQuantity = 'Y' THEN f.EffectiveArea ELSE fc.ApprovedQty END
+
+) SO_CreditLimit, 
 COALESCE(SUM(ft.Amt), 0) SO_CreditUsed, 
 CASE 
 	WHEN o.IsExceedCreditLimit = 'Y' 
@@ -59,6 +69,7 @@ LEFT JOIN FTA_Fact ft ON(ft.FTA_FarmerCredit_ID = o.FTA_FarmerCredit_ID
 				AND ft.FTA_CreditDefinitionLine_ID = cdl.FTA_CreditDefinitionLine_ID AND ft.AD_Table_ID = 318)
 LEFT JOIN M_Product pr ON(pr.M_Product_ID = ol.M_Product_ID) 
 LEFT JOIN C_Charge cr ON(cr.C_Charge_ID = ol.C_Charge_ID) 
+LEFT JOIN FTA_Farming f ON  (f.FTA_FarmerCredit_ID = fc.FTA_FarmerCredit_ID)
 WHERE (
 		(cdl.M_Product_ID = ol.M_Product_ID 
 			AND ol.M_Product_ID IS NOT NULL) 
@@ -72,12 +83,17 @@ WHERE (
 GROUP BY o.AD_Client_ID, o.AD_Org_ID, o.Updated, o.UpdatedBy, o.Created, o.CreatedBy, o.IsActive, 
 o.C_Order_ID, o.C_BPartner_ID, o.DateOrdered, o.DocumentNo, o.Description, 
 cd.FTA_CreditDefinition_ID, cdl.FTA_CreditDefinitionLine_ID, o.FTA_FarmerCredit_ID,  
-o.C_Order_ID, ol.C_OrderLine_ID, ol.LineNetAmt, t.Rate, cdl.Amt, fc.ApprovedQty, cdl.Line, ft.AD_Table_ID 
+o.C_Order_ID, ol.C_OrderLine_ID, ol.LineNetAmt, t.Rate, cdl.Amt, fc.ApprovedQty, cdl.Line, ft.AD_Table_ID ,
+fc.BasedOnEffectiveQuantity,f.EffectiveArea 
 UNION ALL
 SELECT l.AD_Client_ID, l.AD_Org_ID, l.Updated, l.UpdatedBy, l.Created, l.CreatedBy, l.IsActive, 
 l.C_BPartner_ID, l.DateDoc, l.DocumentNo, l.Description::VARCHAR, 
 cd.FTA_CreditDefinition_ID, cdl.FTA_CreditDefinitionLine_ID, l.FTA_FarmerCredit_ID,  
-ll.FTA_FarmerLiquidationLine_ID Line_ID, ll.LineNetAmt Amt, (cdl.Amt * fc.ApprovedQty) SO_CreditLimit, 
+ll.FTA_FarmerLiquidationLine_ID Line_ID, ll.LineNetAmt Amt, 
+--(cdl.Amt * fc.ApprovedQty) SO_CreditLimit, 
+(cdl.Amt * CASE WHEN fc.BasedOnEffectiveQuantity = 'Y' THEN f.EffectiveArea ELSE fc.ApprovedQty END
+
+) SO_CreditLimit, 
 0 SO_CreditUsed,'Y' IsExceedCreditLimit, 
 'N' IsCreditFactManual, 'N' IsSOTrx, cdl.Line, l.FTA_FarmerLiquidation_ID Record_ID, 53564 AD_Table_ID, 0 SeqNo
 FROM FTA_FarmerLiquidation l 
@@ -86,6 +102,7 @@ INNER JOIN FTA_CreditDefinition cd ON(cd.FTA_CreditDefinition_ID = fc.FTA_Credit
 INNER JOIN FTA_FarmerLiquidationLine ll ON(ll.FTA_FarmerLiquidation_ID = l.FTA_FarmerLiquidation_ID) 
 INNER JOIN FTA_CreditDefinitionLine cdl ON(cdl.FTA_CreditDefinition_ID = cd.FTA_CreditDefinition_ID) 
 LEFT JOIN M_Product pr ON(pr.M_Product_ID = l.M_Product_ID) 
+LEFT JOIN FTA_Farming f ON  (f.FTA_FarmerCredit_ID = fc.FTA_FarmerCredit_ID)
 WHERE (cdl.M_Product_ID = l.M_Product_ID 
 		AND l.M_Product_ID IS NOT NULL) 
 	OR (cdl.M_Product_Category_ID = pr.M_Product_Category_ID 
@@ -93,7 +110,8 @@ WHERE (cdl.M_Product_ID = l.M_Product_ID
 GROUP BY l.AD_Client_ID, l.AD_Org_ID, l.Updated, l.UpdatedBy, l.Created, l.CreatedBy, l.IsActive, 
 l.FTA_FarmerLiquidation_ID, l.C_BPartner_ID, l.DateDoc, l.DocumentNo, l.Description, 
 cd.FTA_CreditDefinition_ID, cdl.FTA_CreditDefinitionLine_ID, l.FTA_FarmerCredit_ID,  
-l.FTA_FarmerLiquidation_ID, ll.FTA_FarmerLiquidationLine_ID, ll.LineNetAmt, cdl.Amt, fc.ApprovedQty, cdl.Line
+l.FTA_FarmerLiquidation_ID, ll.FTA_FarmerLiquidationLine_ID, ll.LineNetAmt, cdl.Amt, fc.ApprovedQty, cdl.Line,
+fc.BasedOnEffectiveQuantity,f.EffectiveArea
 UNION ALL
 SELECT a.AD_Client_ID, a.AD_Org_ID, a.Updated, a.UpdatedBy, a.Created, a.CreatedBy, a.IsActive, 
 al.C_BPartner_ID, a.DateDoc, a.DocumentNo, a.DocumentNo || ' <-> ' || l.DocumentNo || ' ' || COALESCE(a.Description, '')::VARCHAR, 
