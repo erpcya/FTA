15542d201356c6e8c76ad91239224ac0ca46aca0
Dixon Martinez <dixon.22martinez@gmail.com>
2014-10-24 17:06:38 -0530

    Add Process Load Order Generate Invoice

--- migration/postgresql/801Add_Columns_To_Default_Tables_Postgres.sql
+++ migration/postgresql/801Add_Columns_To_Default_Tables_Postgres.sql
@@ -117,3 +117,9 @@ ALTER TABLE M_RMA ADD COLUMN FTA_FarmerCredit_ID NUMERIC(10) DEFAULT NULL
 -- FTA
 ALTER TABLE FTA_RecordWeight ADD COLUMN TrailerPlate VARCHAR(60) DEFAULT NULL 
 ;
+
+-- Oct 24, 2014 10:54:35 AM VET
+-- Farming Technical Assistance
+ALTER TABLE C_DocType ADD COLUMN IsCheckReturn CHAR(1) DEFAULT 'N' CHECK (IsCheckReturn IN ('Y','N'))
+;
+
