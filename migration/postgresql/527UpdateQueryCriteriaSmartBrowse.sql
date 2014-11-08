7f9f2839e327adaf89e101345bc87aca76ec55f8
dixon22ma <dixon.22martinez@gmail.com>
2014-11-08 12:44:34 -0430

    Update Query Criteria Smart Browse

---
+++ migration/postgresql/527UpdateQueryCriteriaSmartBrowse.sql
@@ -0,0 +1,55 @@
+-- Nov 7, 2014 8:53:21 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-07 20:53:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53977
+;
+
+-- Nov 7, 2014 8:53:39 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-07 20:53:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53961
+;
+
+-- Nov 7, 2014 8:54:06 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-07 20:54:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53966
+;
+
+-- Nov 7, 2014 8:54:12 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-07 20:54:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53957
+;
+
+-- Nov 7, 2014 8:54:19 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-07 20:54:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53964
+;
+
+-- Nov 7, 2014 8:54:30 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-07 20:54:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53978
+;
+
+-- Nov 7, 2014 8:54:46 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-07 20:54:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53971
+;
+
+-- Nov 7, 2014 8:54:48 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-07 20:54:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53965
+;
+
+-- Nov 7, 2014 8:54:53 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-07 20:54:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53962
+;
+
+-- Nov 7, 2014 8:55:06 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-07 20:55:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53953
+;
+
+-- Nov 7, 2014 8:55:11 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-07 20:55:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53970
+;
+
---
+++ migration/postgresql/528UpdateQueryCriteriaSmartBrowse.sql
@@ -0,0 +1,60 @@
+-- Nov 8, 2014 12:30:26 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-08 12:30:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53912
+;
+
+-- Nov 8, 2014 12:30:32 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-08 12:30:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53899
+;
+
+-- Nov 8, 2014 12:30:37 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-08 12:30:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53887
+;
+
+-- Nov 8, 2014 12:30:41 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-08 12:30:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53904
+;
+
+-- Nov 8, 2014 12:30:45 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-08 12:30:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53900
+;
+
+-- Nov 8, 2014 12:30:50 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-08 12:30:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53896
+;
+
+-- Nov 8, 2014 12:30:52 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-08 12:30:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53895
+;
+
+-- Nov 8, 2014 12:32:41 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-08 12:32:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53911
+;
+
+-- Nov 8, 2014 12:33:47 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-08 12:33:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53898
+;
+
+-- Nov 8, 2014 12:33:51 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-08 12:33:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53905
+;
+
+-- Nov 8, 2014 12:33:54 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsQueryCriteria='N',Updated=TO_TIMESTAMP('2014-11-08 12:33:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53891
+;
+
+-- Nov 8, 2014 12:34:28 PM VET
+-- FTA
+UPDATE AD_Browse_Field SET IsRange='Y',Updated=TO_TIMESTAMP('2014-11-08 12:34:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Browse_Field_ID=53960
+;
+
